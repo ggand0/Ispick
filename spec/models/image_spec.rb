a291bd4ad1ca26e24e0c466aea79fdc4633688fe
丸藤剛大 <pentiumx@macbook.local>
2014-02-18 01:37:37 +0900

    Add image model spec
    Around image_from_url method.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Image do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) { { "title" => "MyString" } }
+
+  describe "image_from_url" do
+
+  	it "assigns argument as data" do
+      image = Image.new valid_attributes
+      image.image_from_url("http://dic.nicovideo.jp/oekaki/344522.png")
+      #image.data.url.should eq("http://dic.nicovideo.jp/oekaki/344522.png")
+      image.data.should_not be_nil
+    end
+  end
+
 end
