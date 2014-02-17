dc2a95a935e20d9b4bb0cd9175f218ae968ad04d
丸藤剛大 <pentiumx@macbook.local>
2014-02-18 01:13:11 +0900

    Delete unneeded actions
    Delete new, create, edit, update action in images_controller.rb
    
    - Delete related specs
    - Setup simplecov gem in spec_helper.rb

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -12,45 +12,6 @@ class ImagesController < ApplicationController
   def show
   end
 
-  # GET /images/new
-  def new
-    @image = Image.new
-  end
-
-  # GET /images/1/edit
-  def edit
-  end
-
-  # POST /images
-  # POST /images.json
-  def create
-    @image = Image.new(image_params)
-
-    respond_to do |format|
-      if @image.save
-        format.html { redirect_to @image, notice: 'Image was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @image }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /images/1
-  # PATCH/PUT /images/1.json
-  def update
-    respond_to do |format|
-      if @image.update(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
