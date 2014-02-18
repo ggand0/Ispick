3c2d24ab89460c00aa4272be7e4ee08146bfd55d
pentiumx <pentiumx@live.jp>
2014-02-19 04:19:16 +0900

    Add a piapro scrapping script
    Which fetches recently uploaded illust in official category.

--- script/scrap_test.rb
+++ script/scrap_test.rb
@@ -7,14 +7,14 @@ page_url = 'http://dic.nicovideo.jp'
 # ニコ百「鹿目まどか」
 url = 'http://dic.nicovideo.jp/a/%E9%B9%BF%E7%9B%AE%E3%81%BE%E3%81%A9%E3%81%8B'
 b = Nokogiri::HTML(open(url))
-p url
+puts url
 
 b.css('img').each do |item|
-    img_url = item['src'].split('?')[0]
-    root_img_url = URI.join(page_url,img_url).to_s
-    p item['title']
+  img_url = item['src'].split('?')[0]
+  root_img_url = URI.join(page_url,img_url).to_s
+  puts item['title']
 
-    image = Image.new(title: item['title'])
-    image.image_from_url root_img_url
-    image.save!
+  image = Image.new(title: item['title'])
+  image.image_from_url root_img_url
+  image.save!
 end
