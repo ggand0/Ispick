51a994b1c6206db5f0df77bde98e398f926201f7
pentiumx <pentiumx@live.jp>
2014-02-19 16:39:57 +0900

    Make scrapping modules
    Execute all individual scripts at scrap.rb.
    - Delete index_list.html
    - Delete an example div in index.html
    - Make child modules in scrapping scripts

---
+++ script/scrap.rb
@@ -0,0 +1,14 @@
+module Scrap
+  require "#{Rails.root}/script/scrap_nico"
+  require "#{Rails.root}/script/scrap_piapro"
+  require "#{Rails.root}/script/scrap_pixiv"
+
+  def self.scrap_all()
+    Scrap::Nico.scrap()
+    Scrap::Piapro.scrap()
+    Scrap::Pixiv.scrap()
+  end
+end
+
+# 抽出開始
+Scrap.scrap_all()
--- script/scrap_piapro.rb
+++ script/scrap_piapro.rb
@@ -1,30 +1,33 @@
-
 # coding: utf-8
 require 'open-uri'
 
-site_url = 'http://piapro.jp/'
+module Scrap::Piapro
+  ROOT_URL = 'http://piapro.jp/'
 
-# piapro
-url = 'http://piapro.jp/illust/?categoryId=3'
-b = Nokogiri::HTML(open(url))
-puts url
+  def self.scrap()
+    url = 'http://piapro.jp/illust/?categoryId=3'
+    b = Nokogiri::HTML(open(url))
+    puts url
 
-b.css("a[class='i_image']").each do |item|
-	page_url = item['href']
-  #puts page_url
-	root_img_url = URI.join(site_url, page_url).to_s
-	#puts root_img_url
+    b.css("a[class='i_image']").each do |item|
+    	page_url = item['href']
+      #puts page_url
+    	root_img_url = URI.join(ROOT_URL, page_url).to_s
+    	#puts root_img_url
 
-  page = Nokogiri::HTML(open(root_img_url))
-  # target : style="background:url(http://c1.piapro.jp/xxx.png) no-repeat center;">
-  str = page.css("div[class='dtl_works dtl_ill']").first
-  #puts str['style']
-  img_url = str['style'][/\((.*?)\)/] # (...)の中のurlを取り出す
-  img_url = img_url.gsub(/[()]/, "")  # ()を除去
-  #img_url = str['style'].match(/\[(.*?)\]/)
-  puts img_url
+      page = Nokogiri::HTML(open(root_img_url))
+      # target : style="background:url(http://c1.piapro.jp/xxx.png) no-repeat center;">
+      str = page.css("div[class='dtl_works dtl_ill']").first
+      #puts str['style']
+      img_url = str['style'][/\((.*?)\)/] # (...)の中のurlを取り出す
+      img_url = img_url.gsub(/[()]/, "")  # ()を除去
+      #img_url = str['style'].match(/\[(.*?)\]/)
+      puts img_url
 
-	image = Image.new(title: item.text)
-	image.image_from_url img_url
-	image.save!
+    	image = Image.new(title: item.text)
+    	image.image_from_url img_url
+    	image.save!
+    end
+  end
+
 end
