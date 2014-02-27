aeaae9feddfcef1a8859c394a62439d8cab9a802
pentiumx <pentiumx@live.jp>
2014-02-28 00:00:32 +0900

    refs #15 Add some factories
    To use them in TargetImageController specs.

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-  factory :image do
+  factory :image_file, class: Image do
     title 'madoka'
+    data { fixture_file_upload('spec/fixtures/files/madoka.png') }
+  end
+
+  factory :image do
+    title 'test'
     sequence(:src_url) { |n| "test#{n}.com" }
   end
 end
--- spec/factories/target_images.rb
+++ spec/factories/target_images.rb
@@ -5,4 +5,9 @@ FactoryGirl.define do
     title 'madoka'
     data { fixture_file_upload('spec/fixtures/files/madoka.png') }
   end
+
+  factory :target_image1, class: TargetImage do
+    title 'madoka1'
+    data { fixture_file_upload('spec/fixtures/files/madoka.png') }
+  end
 end
