eeee24d34c8de54cada2f17a103576778e46241a
pentiumx <pentiumx@live.jp>
2014-02-27 23:58:10 +0900

    refs #15 Add TargetImageService specs
    - Add get_face_feature spec
    - Shorten get_face_feature method's code
    - Stub detect method in prefer method spec

--- spec/services/taget_images_service_spec.rb
+++ spec/services/taget_images_service_spec.rb
@@ -3,15 +3,22 @@ require "#{Rails.root}/app/services/target_images_service"
 include ActionDispatch::TestProcess
 
 describe TargetImagesService do
-  let(:valid_attributes) {{
-    title: "MyString",
-    data: fixture_file_upload('files/madoka.png')
-  }}
+  let(:valid_attributes) { FactoryGirl.attributes_for(:target_image) }
+
+  describe "get_face_feature method" do
+    it "returns face feature array of a image" do
+      target_image = TargetImage.create! valid_attributes
+      AnimeFace.stub(:detect).and_return(['a json array'])
+
+      service = TargetImagesService.new
+      service.get_face_feature(target_image.data.path).should be_an(Array)
+    end
+  end
 
   describe "prefer method" do
-    #specify "returns list of images" do
     it "returns list of images" do
       target_image = TargetImage.create! valid_attributes
+      AnimeFace.stub(:detect).and_return(['a json array'])
 
       service = TargetImagesService.new
       list = service.prefer target_image
