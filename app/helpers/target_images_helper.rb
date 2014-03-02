2bb7a76f5746d381a65f156377e3b01263772575
pentiumx <pentiumx@live.jp>
2014-03-02 23:17:31 +0900

    #21 Fix template rendering bug
    With implementing view helper method.

--- app/helpers/target_images_helper.rb
+++ app/helpers/target_images_helper.rb
@@ -1,2 +1,10 @@
 module TargetImagesHelper
+  require 'kaminari/helpers/paginator'
+  def paginate_zero(array)
+    if not array.count == 0
+      paginate array
+    else
+      'No matches.'
+    end
+  end
 end
