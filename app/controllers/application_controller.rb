a3b45c84a799266b71f96e7d028bd10a16e7d8ef
pentiumx <pentiumx@live.jp>
2014-03-04 15:49:33 +0900

    refs #26 #27 Read authentication information from yml file
    - Ignore config.yml file
    - Use authenticate_or_request_with_http_basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # production環境のみ認証するように
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_NAME'], :password => ENV['BASIC_AUTH_PW'] if Rails.env.production?
+  #http_basic_authenticate_with :name => ENV['BASIC_AUTH_NAME'], :password => ENV['BASIC_AUTH_PW'] if Rails.env.production?
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    if CONFIG['perform_authentication']
+      authenticate_or_request_with_http_basic do |username, password|
+        username == CONFIG['username'] && password == CONFIG['password']
+      end
+    end
+  end
 end
