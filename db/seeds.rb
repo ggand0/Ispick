bce4a3a72e4e9d27157b2c9b24dbaa1f345ed685
pentiumx <pentiumx@live.jp>
2014-03-07 12:51:12 +0900

    refs #29 Seed a database
    With images in spec/files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+include ActionDispatch::TestProcess
+require "#{Rails.root}/app/workers/images_face"
+
+files = Dir["#{Rails.root}/spec/files/images/*"]
+count = 0
+
+
+files.each do |f|
+  image = Image.new(title: 'seed data ' + count.to_s, data: fixture_file_upload(f), src_url: 'seeddata.com/'+count.to_s)
+
+  begin
+    if image.save
+      Resque.enqueue(ImageFace, image.id)
+    else
+      puts 'failed.'
+    end
+  rescue Exception => e
+    puts e
+  end
+  count += 1
+end
