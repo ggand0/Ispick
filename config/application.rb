ba60a2aa03e18d4a31f676412b51c22c7b64e048
pentiumx <pentiumx@live.jp>
2014-03-11 00:56:11 +0900

    Organize Gemfile
    - Add /lib to eager_load path

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Ispic
 
     config.autoload_paths += ["#{Rails.root}/app/services"]
     config.eager_load_paths += ["#{Rails.root}/app/services"]
+    config.eager_load_paths += ["#{Rails.root}/lib"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
