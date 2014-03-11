fcf0c7ae8efd218089c1576ce788975ed16a05f5
pentiumx <pentiumx@live.jp>
2014-03-11 19:24:56 +0900

    refs #45 Implement twitter authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 require 'resque_web'
 
 Ispic::Application.routes.draw do
-  devise_for :users
+  # Devise
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
+
   get "welcome/index"
   resources :target_images do
     member do
