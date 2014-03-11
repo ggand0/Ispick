bd71d2e23c0ed36358c911eb3955c556e4559679
pentiumx <pentiumx@live.jp>
2014-03-11 15:37:36 +0900

    close #44 Add feature.rake specs
    Fix some codes in old tasks.

--- spec/lib/tasks/feature_rake_spec.rb
+++ spec/lib/tasks/feature_rake_spec.rb
@@ -1,4 +1,4 @@
-describe "feature:face_target" do
+describe "feature:face_targets" do
   # 諸々の初期化。gemの仕様的にこれ以上DRYにできない
   before do
     IO.any_instance.stub(:puts)
@@ -6,7 +6,31 @@ describe "feature:face_target" do
   include_context 'rake'
   its(:prerequisites) { should include('environment') }
 
-  it "should call valid a method" do
-    suject.invoke.should_raise
+  it "should call valid a methods" do
+    FactoryGirl.create(:target_image)
+    TargetImagesService.any_instance.stub(:prefer).and_return({result: '[]'})
+    TargetImagesService.any_instance.should_receive(:prefer)
+
+    subject.invoke
+    TargetImage.first.feature.face.should eq ('[]'.to_json)
+  end
+end
+
+describe "feature:face_images" do
+  before do
+    IO.any_instance.stub(:puts)
+  end
+  include_context 'rake'
+  its(:prerequisites) { should include('environment') }
+
+  it "should call valid a methods" do
+    Image.delete_all
+    FactoryGirl.create(:image)
+
+    TargetImagesService.any_instance.stub(:prefer).and_return({result: '[]'})
+    TargetImagesService.any_instance.should_receive(:prefer)
+
+    subject.invoke
+    Image.first.feature.face.should eq ('[]'.to_json)
   end
 end
