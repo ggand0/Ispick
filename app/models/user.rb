83ed43642f8e398346f3729881546f9ddc19cffc
pentiumx <pentiumx@live.jp>
2014-03-12 13:55:43 +0900

    refs #49 Fix a bug that a twitter user cannot login
    Generally it cannot get email adress from twitter. Solve it with generating email string.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ActiveRecord::Base
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"]
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email']
       end
     end
   end
@@ -19,11 +19,12 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name:auth.extra.raw_info.name,
-                          provider:auth.provider,
-                          uid:auth.uid,
-                          # email:auth.info.email, #emailを取得したい場合は、migrationにemailを追加してください。
-                          password:Devise.friendly_token[0,20]
+      user = User.create(
+        name:auth.extra.raw_info.name,
+        provider:auth.provider,
+        uid:auth.uid,
+        # email:auth.info.email, #emailを取得したい場合は、migrationにemailを追加してください。
+        password:Devise.friendly_token[0,20]
       )
     end
     user
@@ -33,14 +34,25 @@ class User < ActiveRecord::Base
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name:auth.info.nickname,
-                          provider:auth.provider,
-                          uid:auth.uid,
-                          # email:auth.extra.user_hash.email, #色々頑張りましたがtwitterではemail取得できません
-                          password:Devise.friendly_token[0,20]
+      user = User.create(
+        name:     auth.info.nickname,
+        provider: auth.provider,
+        uid:      auth.uid,
+        email:    User.create_unique_email,
+        password: Devise.friendly_token[0,20]
       )
     end
     user
+  end
+
+  # 通常サインアップ時のuid用、Twitter OAuth認証時のemail用にuuidな文字列を生成
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
+  # twitterではemailを取得できないので、適当に一意のemailを生成
+  def self.create_unique_email
+    User.create_unique_string + '@example.com'
   end
 
 end
