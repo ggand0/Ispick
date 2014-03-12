720b9b76c75fcb42057664aac42afd79d1fd9b49
pentiumx <pentiumx@live.jp>
2014-03-13 00:34:28 +0900

    refs #50 Add support module for devise auth testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def home
     if signed_in?
-     @images = current_user.delivered_images
-     render :action => 'signed_in'
+      @images = current_user.delivered_images
+      render :action => 'signed_in'
     else
-     render :action => 'not_signed_in'
+      render :action => 'not_signed_in'
     end
   end
+
 end
