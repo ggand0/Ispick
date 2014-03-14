16b3bc04b9e64c11fba3e109a03a0844785c9e15
pentiumx <pentiumx@live.jp>
2014-03-15 01:50:21 +0900

    refs #49 Fix pagination of delivered_images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
   def home
     if signed_in?
-      @images = current_user.delivered_images
-      render :action => 'signed_in'
+      # paginationについては調整中。数が固定されたらモデルに表示数を定義する
+      @images = current_user.delivered_images.page(params[:page]).per(25)
+      render action: 'signed_in'
     else
-      render :action => 'not_signed_in'
+      render action: 'not_signed_in'
     end
   end
 
   def show_target_images
     if signed_in?
       @images = current_user.target_images
-      render :action => 'show_target_images'
+      render action: 'show_target_images'
     else
-      render :action => 'not_signed_in'
+      render action: 'not_signed_in'
     end
   end
 
