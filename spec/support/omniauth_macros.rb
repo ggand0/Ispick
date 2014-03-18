9bc6d5b8f06c449733f12e170616081644dc5bcd
pentiumx <pentiumx@live.jp>
2014-03-18 21:29:53 +0900

    refs #54 Add top page specs
    - Add omniauth_macros module to mock oauth login
    - Add twitter login spec

---
+++ spec/support/omniauth_macros.rb
@@ -0,0 +1,14 @@
+module OmniauthMacros
+  def mock_auth_hash
+    # The mock_auth configuration allows you to set per-provider (or default)
+    # authentication hashes to return during integration testing.
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      :provider => 'twitter',
+      :uid => '123545',
+      info: { nickname: 'pentiumx' }
+      # etc.
+    })
+    #OmniAuth.config.add_mock(:twitter, {:uid => '1234'})
+  end
+end
