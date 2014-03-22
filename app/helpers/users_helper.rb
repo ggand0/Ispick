8159caa020b8c06480224d5476e0c14473278cd6
pentiumx <pentiumx@live.jp>
2014-03-22 15:49:20 +0900

    refs #68 Display favored_images in show_favored_images template
    - Add favored_images/show template
    - Change users_helper functions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,19 +1,32 @@
 module UsersHelper
   def get_clip_string(delivered_image)
-    if not delivered_image.favored
-      'Clip'
+    if delivered_image.favored_image_id
+      'Clipped'
     else
-      'Unclip'
+      'Clip'
     end
   end
 
   def get_clip_string_styled(delivered_image)
     style = ''
-    if not delivered_image.favored
-      style = 'style="color: #000;"'
-    else
+    if delivered_image.favored_image_id
       style = 'style="color: #02C293;"'
+    else
+      style = 'style="color: #000;"'
     end
     '<span '+style+'>' + get_clip_string(delivered_image) + '</span>'
+  end
+
+  def get_total_size_favored(favored_images)
+    total_size = 0
+    favored_images.each do |n|
+      if n.delivered_image_id # まだコピーされていない(生存中のDeliveredImageを参照している)
+        image = DeliveredImage.find(n.delivered_image_id)
+        total_size += image.data.size
+      else                    # 既にコピーされている(ソース元のDeliveredImageは削除されている)
+        total_size += n.data.size
+      end
+    end
+    total_size
   end
 end
