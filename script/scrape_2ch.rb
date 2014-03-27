202acf357cf2ffda2802dc9f93cf20681c1b8ac3
pentiumx <pentiumx@live.jp>
2014-03-27 12:26:58 +0900

    refs #71 Fix 2ch scraping script
    - Ensure that titles are saved properly

--- script/scrape_2ch.rb
+++ script/scrape_2ch.rb
@@ -15,22 +15,30 @@ module Scrape::Nichan
     puts 'Extracting : ' + ROOT_URL
 
     # 2ちゃんねるのベースURL
-    base_url = 'http://toro.2ch.net/illustrator/'    # イラストレータ板
+    #base_url = 'http://toro.2ch.net/illustrator/'    # イラストレータ板
+    boards = [
+      'http://toro.2ch.net/illustrator/',    # イラストレータ板
+      'http://toro.2ch.net/anime/'            # アニメ板
+    ]
 
     # 2ちゃんねるのスレッドのdatファイルURLを取得する
     limit = 5
-    thread_dats = self.get_dat_url(base_url, limit)    # ハッシュ
+    boards.each do |board_url|
+      self.scrape_board(board_url, limit)
+    end
+  end
+
+  def self.scrape_board(board_url, limit)
+    thread_dats = self.get_dat_url(board_url, limit)    # ハッシュ
 
     # 画像URLを取得する
     img_url_array = []    # 空の配列
-    puts thread_dats.class
-    puts thread_dats[:title]
-    thread_dats[:url].each do |url|
+    thread_dats.each do |dat|
       # datファイルの内容を文字列として取得
-      dat_text = Nokogiri::HTML(open(url)).to_s
+      dat_text = Nokogiri::HTML(open(dat[:url])).to_s
       str = "㎡"
       dat_text.delete!(str)   # 環境依存文字を除外
-      img_url_array += self.get_img_url(dat_text)
+      img_url_array.push({ url: self.get_img_url(dat_text), title: dat[:title] })
     end
 
     # URLの重複をなくす
@@ -38,12 +46,14 @@ module Scrape::Nichan
 
     # URLの配列について処理
     # zipで2配列をiterateする：http://goo.gl/6ikMRg
-    img_url_array.zip(thread_dats[:title]).each do |url, title|
-      img_title = self.get_image_name(url)      # 画像のタイトルを決定
-      printf("%s : %s\n", title, url)           # 出力テスト
+    img_url_array.each do |img|
+      img[:url].each do |url|
+        img_title = self.get_image_name(url)      # 画像のタイトルを決定
+        printf("%s : %s\n", img[:title], url)           # 出力テスト
 
-      #Scrape::save_image(img_title, url)        # Imageモデル生成＆DB保存
-      Scrape::save_image(title, url)            # Imageモデル生成＆DB保存
+        #Scrape::save_image(img_title, url)        # Imageモデル生成＆DB保存
+        Scrape::save_image(img[:title], url)            # Imageモデル生成＆DB保存
+      end
     end
   end
 
@@ -53,31 +63,30 @@ module Scrape::Nichan
     subject_url = base_url + 'subject.txt'
 
     # スレッドのURLを取得
-    thread_dat_url = []    # 空の配列
-    thread_dat_title = []
+    thread_dats = []    # 空の配列
     open(subject_url) do |con|
       con.each do |line|
         # datファイルのURLを配列に追加
         if /^(.*\.dat)/ =~ line then        # 正規表現にその行がマッチすれば
           dat_url = base_url + 'dat/' + $1  # マッチした文字列をurlに使用
-          thread_dat_url.push(dat_url)
 
           # "xxx.dat<>"後の文字列がタイトルであると見越して取得
           thread_title = line.match(/.*dat<>/).post_match
-          thread_dat_title.push(thread_title.toutf8)
+
+          # HashのArrayとする
+          thread_dats.push({ url: dat_url, title: thread_title.toutf8 })
         end
 
-        return { url: thread_dat_url, title: thread_dat_title } if thread_dat_url.size >= limit
+        return thread_dats if thread_dats.size >= limit
       end
     end
 
-    { url: thread_dat_url, title: thread_dat_title }
+    thread_dats
   end
 
   # datファイルのテキストから画像のURLを取得し、配列で返す関数
   def self.get_img_url(text)
     img_url = []    # 空の配列
-    #res_text = []
 
     # jpg, png, gif形式以外ならば除外する
     img_url += text.scan(/http:\/\/.*?\.jpg/i)
