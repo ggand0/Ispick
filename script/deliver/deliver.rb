818cd29c5fec31bd6ce5d377ab5f54ba0c091fb0
pentiumx <pentiumx@live.jp>
2014-03-29 18:14:43 +0900

    refs #78 Refactor deliver module
    Use same local functions in deliver_from_image and deliver_from_words function.

--- script/deliver/deliver.rb
+++ script/deliver/deliver.rb
@@ -46,9 +46,12 @@ module Deliver
         src_url: im.src_url,
         data: im.data
       )
-      #im.tags.each { |tag| image.tags << tag }
-      image.targetable = target# target_image / target_word
+      image.targetable = target# target_image or target_word
       if image
+        # file.close出来てもuser.delivered_imagesはclose出来ない
+        # (userがglobalから参照される限りuser.delivered_images[i].dataも参照される)ので、
+        # ファイルへの参照数がタスク終了するまで増加していくことに注意。
+        # 開いているファイル数がulimitで設定されている数を超えると'Too many open files...'エラー
         user.delivered_images << image# ここがcritical
         user.save
       end
@@ -87,11 +90,12 @@ module Deliver
     images.compact!
     puts images.count
 
+    # 配信画像を制限する
     images = self.limit_images(user, images)
 
+    # User.delivered_imagesへ追加する
     self.deliver_images(user, images, target_word)
 
-    puts target_word
     # 最終配信日時を記録
     target_word.last_delivered_at = DateTime.now
   end
@@ -107,39 +111,14 @@ module Deliver
     images = result[:images]
     puts 'Preferred images: ' + images.count.to_s
 
-    # 既に配信済みの画像である場合はskip
-    images.reject! do |x|
-      user.delivered_images.any?{ |d| d.src_url == x[:image].src_url }
-    end
-    puts 'Unique images: ' + images.count.to_s
+    # Hashのarrayではなく単純なImageのarrayにする
+    images = images.map{ |image| image[:image] }
 
-    # 最大配信数に絞る（推薦度の高い順に残す）
-    if images.count > MAX_DELIVER_NUM
-      puts 'Removing excessed images...'
-      images = images.take MAX_DELIVER_NUM
-    end
-    puts 'Final delivered images: ' + images.count.to_s
+    # 配信画像を制限する
+    images = self.limit_images(user, images)
 
     # User.delivered_imagesへ追加
-    c = 0
-    images.each do |i|
-      im = i[:image]
-      file = File.open(im.data.path)
-      image = DeliveredImage.create(title: im.title, src_url: im.src_url)
-      if image
-        # file.close出来てもuser.delivered_imagesはclose出来ない
-        # (userがglobalから参照される限りuser.delivered_images[i].dataも参照される)ので、
-        # ファイルへの参照数がタスク終了するまで増加していくことに注意。
-        # 開いているファイル数がulimitで設定されている数を超えると'Too many open files...'エラー
-        image.data = file
-        user.delivered_images << image# ここがcritical
-        user.save
-      end
-
-      file.close
-      c += 1
-      puts '- Creating delivered_images:' + c.to_s + ' / ' + images.count.to_s if c % 10 == 0
-    end
+    self.deliver_images(user, images, target_image)
 
     # 最終配信日時を記録
     target_image.last_delivered_at = DateTime.now
