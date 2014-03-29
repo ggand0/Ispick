32e13f7dd41ee6e7e0dc325beecb5aa359c853b9
pentiumx <pentiumx@live.jp>
2014-03-29 10:42:23 +0900

    refs #78 Add deliver_from_words function in deliver module
    - Extract some local functions
    - Add related specs

--- script/deliver/deliver.rb
+++ script/deliver/deliver.rb
@@ -7,7 +7,7 @@ module Deliver
   # 1回の配信で、1ユーザーに対して配信する推薦イラストの数
   MAX_DELIVER_NUM = 100
   # [MB]
-  MAX_DELIVER_SIZE = 100
+  MAX_DELIVER_SIZE = 200
 
   def self.deliver(user_id)
     count = 0
@@ -15,13 +15,86 @@ module Deliver
     user = User.find(user_id)
     count_all = user.target_images.length
 
+    user.target_words.each do |t|
+      Deliver.deliver_from_word(user, t)
+    end
     user.target_images.each do |t|
-      Deliver.deliver_from_target(user, t, count_all, count)
+      Deliver.deliver_from_image(user, t, count_all, count)
       count += 1
+    end
+
+    # １ユーザーの最大容量を超えていたら古い順に削除
+    Deliver.delete_excessed_records(user.delivered_images, MAX_DELIVER_SIZE)
+    puts 'Remain delivered_images:' + user.delivered_images.count.to_s
+  end
+
+  def self.contains_word(image, target_word)
+    word = target_word.person.name
+    image.tags.each do |tag|
+      return true if tag.name.include?(word)
+    end
+    image.title.include?(word) or image.caption.include?(word)
+  end
+
+  def self.deliver_images(user, images)
+    # User.delivered_imagesへ追加
+    c = 0
+    images.each do |i|
+      im = i[:image]
+      image = DeliveredImage.create(
+        title: im.title,
+        caption: im.caption,
+        src_url: im.src_url,
+        data: im.data
+      )
+      im.tags.each { |tag| image.tags << tag }
+      if image
+        user.delivered_images << image# ここがcritical
+        user.save
+      end
+      c += 1
+      puts '- Creating delivered_images:' + c.to_s + ' / ' + images.count.to_s if c % 10 == 0
     end
   end
 
-  def self.deliver_from_target(user, target_image, count_all, count)
+  def self.limit_images(user, images)
+    # 既に配信済みの画像である場合はskip
+    images.reject! do |x|
+      user.delivered_images.any?{ |d| d.src_url == x[:image].src_url }
+    end
+    puts 'Unique images: ' + images.count.to_s
+
+    # 最大配信数に絞る（推薦度の高い順に残す）
+    if images.count > MAX_DELIVER_NUM
+      puts 'Removing excessed images...'
+      images = images.take MAX_DELIVER_NUM
+    end
+    puts 'Final delivered images: ' + images.count.to_s
+    images
+  end
+
+  def self.deliver_from_word(user, target_word)
+    # 単純に、title, caption, tagに文字列が含まれているかどうか調べる
+    images = Image.joins(:tags).where.not(title: nil, caption: nil, tags: { name: nil })
+    puts images.count
+
+    # target_wordに、何らかの文字情報が部分一致するimageがあれば残す
+    images = images.map do |image|
+      self.contains_word(image, target_word) ? image : nil
+    end
+    images.compact!
+    puts images.count
+
+    images = self.limit_images(user, images)
+
+    self.deliver_images(user, images)
+
+    puts target_word
+    # 最終配信日時を記録
+    target_word.last_delivered_at = DateTime.now
+  end
+
+  def self.deliver_from_image(user, target_image, count_all, count)
     # Log
     puts 'Processing ' + (count+1).to_s + ' / ' + count_all.to_s
     puts 'User.delivered_images.count: ' + user.delivered_images.count.to_s
@@ -65,10 +138,6 @@ module Deliver
       c += 1
       puts '- Creating delivered_images:' + c.to_s + ' / ' + images.count.to_s if c % 10 == 0
     end
-
-    # １ユーザーの最大容量を超えていたら古い順に削除
-    Deliver.delete_excessed_records(user.delivered_images, MAX_DELIVER_SIZE)
-    puts 'Remain delivered_images:' + user.delivered_images.count.to_s
 
     # 最終配信日時を記録
     target_image.last_delivered_at = DateTime.now
--- spec/scripts/deliver/deliver_spec.rb
+++ spec/scripts/deliver/deliver_spec.rb
@@ -6,7 +6,7 @@ include ApplicationHelper
 describe "Deliver" do
   describe "delete_excessed_records" do
     before do
-      IO.any_instance.stub(:puts)
+      #IO.any_instance.stub(:puts)
     end
     it "delete images properly" do
       FactoryGirl.create(:user_with_delivered_images, images_count: 5)
@@ -18,6 +18,44 @@ describe "Deliver" do
       User.first.delivered_images.each do |d|
         puts d.created_at
       end
+    end
+  end
+
+  describe "contains_word function" do
+    it "returns true if some column matches" do
+      # タグに「鹿目まどか」という名前を持つものがあるimageを作成する
+      image = FactoryGirl.create(:image_with_tags, tags_count: 5)
+      person = FactoryGirl.create(:person_madoka)
+      target_word = TargetWord.find(person.target_word_id)
+      # 鹿目まどか」なるtarget_word
+
+      contains = Deliver.contains_word(image, target_word)
+      expect(contains).to eq(true)
+    end
+
+    it "returns false if no matches" do
+      # 全く登録タグに関する情報が無いimage
+      image = FactoryGirl.create(:image)
+      person = FactoryGirl.create(:person_madoka)
+      target_word = TargetWord.find(person.target_word_id)
+
+      contains = Deliver.contains_word(image, target_word)
+      expect(contains).to eq(false)
+    end
+  end
+
+  describe "limit_images function" do
+
+  end
+
+  describe "deliver_images function" do
+
+  end
+
+  describe "deliver_from_word function" do
+    it "deliver properly" do
+      FactoryGirl.create(:user_with_target_words, words_count: 5)
+      Deliver.deliver_from_word(1, User.first.target_words.first)
     end
   end
 end
