cad5673aaf4882d52ae2c13becbf7ba709d0ce40
pentiumx <pentiumx@live.jp>
2014-04-04 03:46:13 +0900

    refs #14 Fix illust detection worker class
    - Add specs

--- app/workers/detect_illust.rb
+++ app/workers/detect_illust.rb
@@ -2,15 +2,24 @@ class DetectIllust
   # Woeker起動時に指定するQUEUE名
   @queue = :detect_illust
 
+  # イラスト判定ツールを実行し結果を得る
+  def self.get_result(tool_path, image)
+    %x(#{tool_path} #{image.data.path})#2>&1
+  end
+
+  # イラストかどうか判定する
   def self.perform(image_id)
     image = Image.find(image_id)
-    tool_path = "#{Rails.root}/lib/opencv.exe"
+    tool_path = CONFIG['illust_detection_path']
+    illust = self.get_result(tool_path, image).to_i
 
-    # イラストかどうか判定する
-    puts image.data.path
-    illust = %x(#{tool_path} #{image.data.path} 2>&1)
-    puts illust
-    is_illust = (illust == 1 ? true : false)
+    begin
+      is_illust = (illust == 1 ? true : false)
+    rescue => e
+      puts e
+      Rails.logger.error('Illust detection failed!')
+      is_illust = false
+    end
     image.update_attributes({is_illust: is_illust})
 
     puts 'ILLUST DETECTION DONE!'
--- spec/workers/detect_illust_spec.rb
+++ spec/workers/detect_illust_spec.rb
@@ -2,20 +2,52 @@ require 'spec_helper'
 require "#{Rails.root}/app/workers/detect_illust"
 
 describe DetectIllust do
-  let(:valid_attributes) { FactoryGirl.attributes_for(:image_file) }
+  let(:valid_attributes) { FactoryGirl.attributes_for(:image) }
 
   before do
-    #IO.any_instance.stub(:puts)
+    IO.any_instance.stub(:puts)
+  end
+
+  describe "get_result method" do
+    it "execute the tool and returns value" do
+      tool_path = CONFIG['illust_detection_path']
+      image = Image.create! valid_attributes
+
+      DetectIllust.should_receive(:`).once.with("#{tool_path} #{image.data.path}")
+
+      DetectIllust.get_result(tool_path, image)
+    end
   end
 
   describe "perform method" do
-    it "should create a new Feature model" do
+    it "updates 'is_illust' column with true when result is '1'" do
       image = Image.create! valid_attributes
+      DetectIllust.stub(:get_result).and_return('1')
 
       detect = DetectIllust.new
       DetectIllust::perform(image.id)
 
-      image.is_illust.should eq(true)
+      expect(Image.find(image.id).is_illust).to eq(true)
+    end
+
+    it "updates 'is_illust' column with false when result is '0'" do
+      image = Image.create! valid_attributes
+      DetectIllust.stub(:get_result).and_return('0')
+
+      detect = DetectIllust.new
+      DetectIllust::perform(image.id)
+
+      expect(Image.find(image.id).is_illust).to eq(false)
+    end
+
+    it "updates 'is_illust' column with false when fails to get valid result" do
+      image = Image.create! valid_attributes
+      DetectIllust.stub(:get_result).and_return('invalid return value')
+
+      detect = DetectIllust.new
+      DetectIllust::perform(image.id)
+
+      expect(Image.find(image.id).is_illust).to eq(false)
     end
   end
 end
