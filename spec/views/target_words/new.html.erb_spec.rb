67381f55c8c93591f44730d5303e054ef8386c2d
pentiumx <pentiumx@live.jp>
2014-04-13 00:28:21 +0900

    refs #95 #104 Fix view specs

--- spec/views/target_words/new.html.erb_spec.rb
+++ spec/views/target_words/new.html.erb_spec.rb
@@ -16,8 +16,11 @@ describe "target_words/new" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", search_target_words_path, "post" do
-      assert_select "input#q_name_display_cont[name=?]", "q[name_display_cont]"
+    #assert_select "form[action=?][method=?]", search_target_words_path, "post" do
+    #  assert_select "input#q_name_display_cont[name=?]", "q[name_display_cont]"
+    #end
+    assert_select "form[action=?][method=?]", target_words_path, "post" do
+      assert_select "input#target_word_word[name=?]", "target_word[word]"
     end
   end
 end
