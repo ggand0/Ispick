632bb0a81cab3014b0db4b0c661fa2aa26b96528
pentiumx <pentiumx@live.jp>
2014-04-13 17:40:30 +0900

    refs #105 Add copy_image worker class

--- app/workers/copy_image.rb
+++ app/workers/copy_image.rb
@@ -3,16 +3,21 @@ class CopyImage
   # Woeker起動時に指定するQUEUE名
   @queue = :copy_image
 
-  # 画像をDLする
-  def self.perform(image_id, data)
+  # 画像をcopyする
+  def self.perform(delivered_image_id, image_id)
     image = Image.find(image_id)
+    delivered_image = DeliveredImage.find(delivered_image_id)
+    delivered_image.data.destroy
 
     begin
-      image.data = data
-      image.save!
+      #puts delivered_image.data
+      puts image.data
+      delivered_image.data = image.data
+      delivered_image.save!
+      puts DeliveredImage.find(delivered_image_id).data
     rescue => e
       puts e
-      Rails.logger.error('Image download failed!')
+      Rails.logger.error('Image copy failed! Needs to be re-delivered.')
       return
     end
 
