12564a1c52687ef262d21608e0c3c4737b936b74
pentiumx <pentiumx@live.jp>
2014-04-13 17:42:10 +0900

    refs #105 Add search_images worker class
    - Fix search_keyword method
    - Fix get_total_size helper method

--- app/models/target_word.rb
+++ app/models/target_word.rb
@@ -1,4 +1,6 @@
 require "#{Rails.root}/script/scrape/scrape"
+require "#{Rails.root}/script/deliver/deliver"
+require "#{Rails.root}/app/workers/search_images"
 
 class TargetWord < ActiveRecord::Base
   belongs_to :user
@@ -8,7 +10,9 @@ class TargetWord < ActiveRecord::Base
   after_create :search_keyword
 
   def search_keyword
-    query = self.person ? self.person.name : self.word
-    Scrape.scrape_keyword(query)
+    #query = self.person ? self.person.name : self.word
+    #Scrape.scrape_keyword(query)
+    #Deliver.deliver_keyword(self.user_id, self.id)
+    Resque.enqueue(SearchImages, self.id)
   end
 end
