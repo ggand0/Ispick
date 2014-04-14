a73878b4f71a0cc4e4ca443e2864c08a5d0847e0
pentiumx <pentiumx@live.jp>
2014-04-14 22:57:30 +0900

    refs #105 Skip target_word callbacks during testing
    Edit target_word factories.

--- spec/factories/target_words.rb
+++ spec/factories/target_words.rb
@@ -5,6 +5,8 @@ FactoryGirl.define do
     word '鹿目 まどか（かなめ まどか）'
     enabled true
 
+    after(:build) { |target_word| target_word.class.skip_callback(:create, :after, :search_keyword) }
+
     factory :word_with_delivered_images do
       ignore do
         images_count 5
@@ -12,6 +14,7 @@ FactoryGirl.define do
       after(:create) do |target_word, evaluator|
         create_list(:delivered_image_with_targetable, evaluator.images_count, targetable: target_word)
       end
+      after(:build) { |target_word| target_word.class.skip_callback(:create, :after, :search_keyword) }
     end
 
     factory :word_with_person do
@@ -21,6 +24,7 @@ FactoryGirl.define do
       after(:build) do |target_word, evaluator|
         create_list(:person_madoka, evaluator.words_count, target_word: target_word)
       end
+      after(:build) { |target_word| target_word.class.skip_callback(:create, :after, :search_keyword) }
     end
   end
 
