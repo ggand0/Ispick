6f6c71ab72afe5d6112a336eeaad3fab5f352007
pentiumx <pentiumx@live.jp>
2014-04-17 19:38:49 +0900

    refs #107 Fix shell scripts that handle resque daemons

--- script/start_all_daemons.sh
+++ script/start_all_daemons.sh
@@ -3,7 +3,7 @@
 # 全てのdaemonを起動する
 #script/daemons/image_face start
 #script/daemons/target_face start
-script/daemons/detect_illust start
-script/daemons/download_image start
-script/daemons/copy_image start
-script/daemons/search_images start
+script/daemons/detect_illust start RAILS_ENV=production
+script/daemons/download_image start RAILS_ENV=production
+script/daemons/copy_image start RAILS_ENV=production
+script/daemons/search_images start RAILS_ENV=production
