b6b53d4d78e0733a9601801e6967eed7771ff7f1
pentiumx <pentiumx@live.jp>
2014-05-01 23:56:31 +0900

    refs #152 Associate image and delivered_image model with has_one relationship

--- app/models/delivered_image.rb
+++ app/models/delivered_image.rb
@@ -3,6 +3,7 @@ class DeliveredImage < ActiveRecord::Base
   belongs_to :favored_image
   belongs_to :targetable, polymorphic: true
   has_one :feature, as: :featurable
+  has_one :image
 
   # 明示的にテーブル名を指定することでエラー回避している
   default_scope { order("#{table_name}.created_at DESC") }
@@ -15,7 +16,7 @@ class DeliveredImage < ActiveRecord::Base
     use_timestamp: false
 
   # Imageモデルで一度validateされているはずだが、一応定義
-  validates_uniqueness_of :src_url
+  #validates_uniqueness_of :src_url
 
   # 後でmodule化する
   def image_from_url(url)
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ActiveRecord::Base
   has_one :feature, as: :featurable
   has_and_belongs_to_many :tags
+  belongs_to :delivered_image
 
   # 明示的にテーブル名を指定することでエラー回避
   default_scope { order("#{table_name}.created_at DESC") }
