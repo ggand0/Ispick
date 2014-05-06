5d11a66a20bddc3caecee815da0203c474a7f589
pentiumx <pentiumx@live.jp>
2014-05-06 12:19:21 +0900

    refs #155 Fix the joining table
    - Turn off validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,6 @@ class Person < ActiveRecord::Base
   belongs_to :target_word
   has_many :keywords
   has_and_belongs_to_many :titles
+
+  #validates_uniqueness_of :name
 end
--- db/migrate/20140505152023_create_people_titles.rb
+++ db/migrate/20140505152023_create_people_titles.rb
@@ -1,6 +1,6 @@
-class CreateTitlePerson < ActiveRecord::Migration
+class CreatePeopleTitles < ActiveRecord::Migration
   def change
-    create_table :title_people, id: false do |t|
+    create_table :people_titles, id: false do |t|
       t.integer :title_id, null: false
       t.integer :person_id, null: false
     end
