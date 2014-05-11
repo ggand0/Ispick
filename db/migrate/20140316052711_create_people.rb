230b68803a0b31df95926a0eb80033c20ba47c0b
pentiumx <pentiumx@live.jp>
2014-05-12 04:34:16 +0900

    refs #156 Add 'name_english' column to 'people' table

--- db/migrate/20140316052711_create_people.rb
+++ db/migrate/20140316052711_create_people.rb
@@ -2,7 +2,8 @@ class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
       t.string :name            # ”鹿目まどか”
-      t.string :name_display    # "鹿目まどか（かなめ まどか）"
+      t.string :name_display    # "鹿目 まどか"
+      t.string :name_english    # "Madoka Kaname"
       t.string :name_type       # "Character"
       t.integer :target_word_id
 
