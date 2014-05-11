18346e7a4ef3092e3c3a8d9c080be86cf22654fd
pentiumx <pentiumx@live.jp>
2014-05-12 04:14:14 +0900

    refs #156 Fix codes related to 'get_anime_character_name' function
    To attach english name of anime characters!

--- script/scrape/scrape_characters.rb
+++ script/scrape/scrape_characters.rb
@@ -79,6 +79,7 @@ module Scrape::Wiki::Character
     Hash[ anime_character_page_url.sort_by{|k,v| k} ]
   end
 
+
   def self.get_character_name_ja(anime_title, html)
     name_array = []
 
@@ -106,19 +107,25 @@ module Scrape::Wiki::Character
                 # '(', '（'のどれかで括られているキャラクタ名の場合
                 # 鹿目 まどか （かなめ まどか）などの表記が該当
                 if /(.*?)[\(（](.*?)[\)）]/ =~ tmp
-                  puts tmp
+=begin
                   first_array = $1.split('/')#／／/
                   second_array = $2.split('/')
                   first_array.map! { |value| value.strip }
                   second_array.map! { |value| value.strip }
                   tmp_array += first_array
                   tmp_array += second_array
+=end
+                  name = $1
+                  _alias = $2
+                  tmp_hash = { name: name, query: name.gsub(/\s/, ''), _alias: _alias }
+                  name_array.push(tmp_hash)
                 # それ以外：「キュウべえ」など
                 else
-                  #puts tmp
-                  tmp_array.push(tmp.strip)
+                  tmp_hash = { name: tmp, query: tmp.strip, _alias: '' }
+                  #tmp_array.push(tmp.strip)
+                  name_array.push(tmp_hash)
                 end
-                name_array.push(tmp_array)
+                #name_array.push(tmp_array)
               end
             end
           elsif current.respond_to?(:name) and (current.name == 'h2' or current.name == 'script')
@@ -143,15 +150,39 @@ module Scrape::Wiki::Character
     end
   end
 
-  # @param [String] 'Madoka Kaname (鹿目 まどか, Kaname Madoka)
+  # 日本語のキャラクタ一覧とマッチするキャラクタかどうか調べる
+  # @param [String] '(鹿目 まどか, Kaname Madoka)'など括弧内の記述部
+  # @return [Hash] { match: boolean, list: 削除後のcharacters_list }
   def self.match_character_name(name_string, characters_list)
+    return '' if characters_list.nil?
+
     characters_list.each do |character_name|
-      if character_name =~ name_string
-        characters_list.delete(character_name)
-        return { match: true, list: characters_list }
+      if /#{character_name[:name]}/ =~ name_string
+        #characters_list.delete(character_name)
+        #return { match: true, list: characters_list }
+        return character_name
       end
     end
-    { match: false, list: characters_list }
+    ''
+  end
+
+  def self.convert_macrons(name)
+    name.gsub!(/ā/, 'aa')
+    name.gsub!(/ī/, 'ii')
+    name.gsub!(/ū/, 'uu')
+    name.gsub!(/ē/, 'ei')
+    name.gsub!(/ō/, 'ou')
+    name
+  end
+
+  def self.add_character_name(name, res, name_array, characters_list)
+    if not res.empty?
+      characters_list.delete(res)
+      res[:en] = self.convert_macrons(name)
+      name_array.push(res)
+    end
+
+    [ name_array, characters_list ]
   end
 
   def self.get_character_name_en(anime_title, html, characters_list)
@@ -166,37 +197,50 @@ module Scrape::Wiki::Character
         while true
           if current.respond_to?(:name) and current.name == 'dl'
             current.css('dt').each do |dt|
+              next if dt.inner_text.empty?
 
-              if not dt.inner_text == ''
-                tmp = dt.inner_text
-                tmp_array = []
-                #result = self.match_character_name(tmp, characters_list)
-                #characters_list = result[:list]
+              tmp = dt.inner_text
+              tmp.gsub!(/\?/, '')
+              tmp_array = []
 
-                tmp.gsub!(/\?/, '')
+              # Leysritt (リーゼリット Rīzeritto?) and Sella (セラ Sera?)
+              if /(.*?) \((.*?)\) and (.*?) \((.*?)\)/ =~ tmp
+                tmp_array = tmp.split(' and ')
+                names = [ $1, $2, $3, $4 ]
 
-                # Leysritt (リーゼリット Rīzeritto?) and Sella (セラ Sera?)
-                #if /(.*?)\((.*?)\)( |\s)and( |\s)(.*?)\((.*?)\)/ =~ tmp
-                if /(.*?) \((.*?)\) and (.*?) \((.*?)\)/ =~ tmp
-                  tmp_array = tmp.split(' and ')
+                res = self.match_character_name(names[1], characters_list)
+                name_array, characters_list = self.add_character_name(
+                  names[0], res, name_array, characters_list) if not res.empty?
 
-                  name_array.push($1)
-                  name_array.push($3)
-                # Yūri (ユウリ?) / Airi Anri (杏里 あいり Anri Airi?)
-                elsif /(.*?) \((.*?)\) \/ (.*?) \((.*?)\)/ =~ tmp
-                  tmp_array = tmp.split(' / ')
+                res = self.match_character_name(names[3], characters_list)
+                name_array, characters_list = self.add_character_name(
+                    names[2], res, name_array, characters_list) if not res.empty?
+              # Yūri (ユウリ?) / Airi Anri (杏里 あいり Anri Airi?)
+              elsif /(.*?) \((.*?)\) \/ (.*?) \((.*?)\)/ =~ tmp
+                tmp_array = tmp.split(' / ')
+                names = [ $1, $2, $3, $4 ]
 
-                  name_array.push($1)
-                  name_array.push($3)
+                res = self.match_character_name(names[1], characters_list)
+                name_array, characters_list = self.add_character_name(
+                  names[0], res, name_array, characters_list) if not res.empty?
+
+                res = self.match_character_name(names[3], characters_list)
+                name_array, characters_list = self.add_character_name(
+                    names[2], res, name_array, characters_list) if not res.empty?
+                res = self.match_character_name(names[1], characters_list)
+
+              else
+                # Madoka Kaname (鹿目 まどか Kaname Madoka?)
+                if /(.*?) \((.*?)\)/ =~ tmp
+                  names = [ $1, $2 ]
+
+                  res = self.match_character_name(names[1], characters_list)
+                  name_array, characters_list = self.add_character_name(
+                    names[0], res, name_array, characters_list) if not res.empty?
                 else
-                  # Madoka Kaname (鹿目 まどか Kaname Madoka?)
-                  if /(.*?) \((.*?)\)/ =~ tmp
-                  #if result[:match]
-                    tmp_array += $1.split('/')
-                  else
-                    tmp_array.push(tmp.strip)
-                  end
-                  name_array.push(tmp_array)
+                  res = self.match_character_name(tmp, characters_list)
+                  name_array, characters_list = self.add_character_name(
+                    tmp, res, name_array, characters_list) if not res.empty?
                 end
               end
 
@@ -219,7 +263,7 @@ module Scrape::Wiki::Character
     # 条件を満たした場合、ハッシュに値を追加
     if not anime_title == '' and not name_array.size == 0
       #anime_character[anime] = name_array
-      return name_array
+      return name_array + characters_list
     end
   end
 
@@ -232,11 +276,13 @@ module Scrape::Wiki::Character
     # 与えられたWikipediaのURLから登場人物の詳細ページを抜き出す
     wiki_url.each do |anime_title, url|
       name_array = []
-      html = Scrape::Wiki.open_html url
+      html_ja = Scrape::Wiki.open_html url[:ja]
+      html_en = Scrape::Wiki.open_html url[:en]
 
-      name_ja = self.get_character_name_ja anime_title, html
-      name_en = self.get_character_name_en anime_title, html
-      anime_character[anime_title] = { ja: name_ja, en: name_en }
+      name_ja = self.get_character_name_ja anime_title, html_ja# [ ['鹿目 まどか', 'かなめ まどか'], ... ]
+      name_hash = self.get_character_name_en anime_title, html_en, name_ja# 英名追加後のHashが返される
+
+      anime_character[anime_title] = name_hash
     end
 
     anime_character
--- spec/scripts/scrape/scrape_characters_spec.rb
+++ spec/scripts/scrape/scrape_characters_spec.rb
@@ -7,7 +7,11 @@ include Scrape::Wiki::Character
 describe "Scrape::Wiki::Character" do
   describe "get_anime_character_name function" do
     it "returns a valid Hash value" do
+      url_ja = 'http://ja.wikipedia.org/wiki/%E9%AD%94%E6%B3%95%E5%B0%91%E5%A5%B3%E3%81%BE%E3%81%A9%E3%81%8B%E2%98%86%E3%83%9E%E3%82%AE%E3%82%AB%E3%81%AE%E3%82%AD%E3%83%A3%E3%83%A9%E3%82%AF%E3%82%BF%E3%83%BC%E4%B8%80%E8%A6%A7'
+      url_en = 'http://en.wikipedia.org/wiki/List_of_Puella_Magi_Madoka_Magica_characters'
+      wiki_url = { '魔法少女まどか☆マギカ' => { ja: url_ja, en: url_en } }
 
+      puts result = Scrape::Wiki::Character.get_anime_character_name(wiki_url)
     end
   end
 
@@ -18,6 +22,9 @@ describe "Scrape::Wiki::Character" do
       html = Scrape::Wiki.open_html url
 
       result = Scrape::Wiki::Character.get_character_name_ja('魔法少女まどか☆マギカ', html)
+      expect(result).to be_an(Array)
+      #result.each { |n| puts n.class.name } # => Array
+      result.each { |n| puts n }
     end
   end
 
@@ -26,11 +33,30 @@ describe "Scrape::Wiki::Character" do
       # 登場人物一覧ページ
       url = 'http://en.wikipedia.org/wiki/List_of_Puella_Magi_Madoka_Magica_characters'
       html = Scrape::Wiki.open_html url
+      characters_list = [
+        {:name=>"鹿目 まどか", :query=>"鹿目まどか", :_alias=>"かなめ まどか"},
+        {:name=>"暁美 ほむら", :query=>"暁美ほむら", :_alias=>"あけみ ほむら"},
+        {:name=>"美樹 さやか", :query=>"美樹さやか", :_alias=>"みき さやか"},
+        {:name=>"巴 マミ", :query=>"巴マミ", :_alias=>"ともえ マミ"},
+        {:name=>"佐倉 杏子", :query=>"佐倉杏子", :_alias=>"さくら きょうこ"},
+        {:name=>"キュゥべえ", :query=>"キュゥべえ", :_alias=>""}
+      ]
 
-      puts result = Scrape::Wiki::Character.get_character_name_en('魔法少女まどか☆マギカ', html, [])
+      puts result = Scrape::Wiki::Character.get_character_name_en(
+        '魔法少女まどか☆マギカ', html, characters_list)
       #result.each { |n| puts "#{n.count}, #{n}" }
     end
   end
+
+  describe "match_character_name function" do
+    it "returns a valid hash" do
+      name_string = '(鹿目 まどか, Kaname Madoka)'
+      characters_list =['鹿目 まどか', '美樹 さやか']
+      puts result = Scrape::Wiki::Character.match_character_name(name_string, characters_list)
+      expect(result).to eq({ match: true, list: ['美樹 さやか']})
+    end
+  end
+
 
   describe "get_anime_character_page function" do
     it "returns a valid hash" do
