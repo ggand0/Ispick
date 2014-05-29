ccd13d80959df7a1e8e83bae026b7edc53dd566c
pentiumx <pentiumx@live.jp>
2014-05-30 03:30:44 +0900

    refs #8 Add specs of Scrape::Fourchan module

--- script/scrape/scrape_4chan.rb
+++ script/scrape/scrape_4chan.rb
@@ -7,36 +7,27 @@ require 'securerandom'
 
 # 4chanから2次画像を抽出する
 module Scrape::Fourchan
-
-  # 4chanURL
   ROOT_URL = 'http://www.4chan.org/'
 
-  # 関数定義
   def self.scrape
-    puts 'Extracting : ' + ROOT_URL
-
-    # 変数
     board = 'c'       # 板の名称(タイトルではない)
     limit = 15        # thread数のlimit。1page 15threads
-    #image_limit = 50
 
     # スレッドの内容を取得する
+    puts 'Extracting : ' + ROOT_URL
     thread_id = self.get_thread_id_list(board, limit)
     thread_post = self.get_thread_post_list(board, thread_id)
     image_url = self.get_image_url_list(board, thread_post)
-    puts "count: #{image_url.length.to_s}"
+    puts "count: #{image_url.length.to_s}" if image_url
 
-    # Imageモデル生成＆DB保存
-    count = 0
+    # Imageレコード作成＆DB保存
     image_url.each do |value|
-      img_name = self.get_image_name(value[:src_url])
-      value[:title] = img_name
-      printf("%s : %s\n", img_name, value[:src_url])
+      value[:title] = self.get_image_name(value[:src_url])
+      is_large = value[:is_large]
+      value.delete(:is_large)
+      puts "#{value[:title]} : #{value[:src_url]}"
 
-      # image_limitの枚数分抽出する
-      success = Scrape::save_image(value)
-      count += success ? 1 : 0
-      #break if count >= image_limit
+      success = Scrape::save_image(value, [], true, is_large)
     end
   end
 
@@ -56,6 +47,7 @@ module Scrape::Fourchan
 
   # 板のスレッドID一覧の配列を取得する関数
   # @param [String] boardを表すcharacter('c'など)
+  # @param [Integer] 取得するboard数
   # @return [Array] スレッドID一覧の配列
   def self.get_thread_id_list(board, limit)
     url = "http://a.4cdn.org/%s/threads.json" % [board]
@@ -87,9 +79,20 @@ module Scrape::Fourchan
       url = "http://a.4cdn.org/%s/res/%s.json" % [board, id]
       json = open(url).read
       data = JSON.parse(json)
-      thread_post_list.push({ posts: data['posts'], id: id })
+      thread_post_list.push({ posts: data['posts'], id: id, tag: self.get_tag_from_comment(data['posts']) })
     end
     thread_post_list
+  end
+
+  # 1レス目のtitle/captionからタグ付けする、
+  # 出来た場合は全てのレスに対して同じタグを付ける
+  # @param [Hash] １つのスレッド内の全てのpost
+  def self.get_tag_from_comment(posts_hash)
+    title = posts_hash.first['com']
+    caption = posts_hash.first['com']
+
+    # TODO: text analyser で英名を抜き出す
+    []
   end
 
   # スレッドの内容の配列から画像URLを取得する関数
@@ -100,16 +103,19 @@ module Scrape::Fourchan
     image_url = []
     thread_post.each do |post|
       post[:posts].each do |value|
-        if value.has_key?('tim') and value.has_key?('ext') then
+        if value.has_key?('tim') and value.has_key?('ext')# then
           url = "http://i.4cdn.org/%s/src/%s%s" % [board, value['tim'], value['ext']]
-          com = value['com'] if value.has_key?('com')
-          posted_at = self.get_posted_at(value)
+          caption = value['com'] if value.has_key?('com')
+          posted_at = self.get_posted_at(value['now'])
+          #is_large = true if (value['fsize'] / (1024.0*1024.0)) > 2　 # 2MB以上ならlarge用のqueueへ
+          is_large = false
 
           image_data = {
             src_url: url,
             page_url: "http://boards.4chan.org/%s/res/%d" % [board, post[:id]],
-            caption: com,
-            posted_at: posted_at
+            caption: caption,
+            posted_at: posted_at,
+            is_large: is_large
           }
 
           image_url.push(image_data)
@@ -120,10 +126,9 @@ module Scrape::Fourchan
   end
 
   # 投稿日時を取得する
-  # @param [Hash] APIの結果
+  # @param [String] '03\/24\/14(Mon)16:09'など
   # @return [Time] UTC時
-  def self.get_posted_at(value)
-    time_string = value['now']                                      # => 03\/24\/14(Mon)16:09
+  def self.get_posted_at(time_string)
     date = time_string.match(/.*\(/).to_s.delete!('(').split('/')   # => 03, 24, 14
     month = date[0].to_i
     day = date[1].to_i
@@ -143,7 +148,7 @@ module Scrape::Fourchan
       return image_name
     else
       image_name = SecureRandom.random_number(10**14)  # ランダムな14桁の数値
-      return image_name
+      return image_name.to_s
     end
   end
 
