c50f2555ebcbc24ea826fe08bdc5b621d351a6a0
pentiumx <pentiumx@live.jp>
2014-05-31 18:00:49 +0900

    refs #8 Add test codes of Scrape::Futaba module

--- script/scrape/scrape_futaba.rb
+++ script/scrape/scrape_futaba.rb
@@ -3,93 +3,48 @@ require 'nokogiri'
 require 'open-uri'
 require 'kconv'
 require 'futaba'
-#require "#{Rails.root}/lib/futaba"
 
-# ふたばちゃんねるから2次画像を抽出する
+
 module Scrape::Futaba
-
-  # ふたばちゃんねるURL
   ROOT_URL = 'http://www.2chan.net/'
 
-  # 関数定義
-  def self.scrape()
-    puts 'Extracting : ' + ROOT_URL
+  # ふたばちゃんねるから2次画像を抽出する
+  def self.scrape
+    puts "Extracting: #{ROOT_URL}"
 
+    threads = self.get_threads
+    self.scrape_threads threads, 5
+  end
+
+  def self.get_threads
     board = Futaba::Board.new('http://dat.2chan.net/img2/')
-    threads = board.catalog.threads()
-    threads.each do |thread|
-      thread.posts().each do |post|
-        #puts "#{post.title} #{post.date} #{post.image} #{post.body} "
+    board.catalog.threads
+  end
+
+  def self.scrape_threads(threads, limit)
+    threads.each_with_index do |thread, count|
+      thread.posts.each do |post|
         if post.image
-          image_data = {
-            title: post.title,
-            caption: post.body,
-            site_name: '2chan',
-            src_url: post.image.uri,
-            page_url: thread.uri,
-            posted_at: post.date
-          }
-          puts printf("%s : %s", image_data[:site_name], image_data[:src_url])
-          Scrape::save_image(image_data)
+          image_data = self.get_data thread, post
+
+          success = Scrape::save_image(image_data)
+          puts "#{image_data[:site_name]} : #{image_data[:src_url]}" if success
         end
       end
-    end
 
-    # self.scrape_board()
-  end
-
-  def self.scrape_board()
-    # 画像URLを取得する
-    url = 'http://dat.2chan.net/img2/futaba.htm'
-    html = Nokogiri::HTML(open(url))
-
-    # 画像URLの配列
-    img_url_array = []
-
-    # aタグ内の画像URLを取得
-    html.css("a[target='_blank']").each do |item|
-      # 画像URL
-      img_url = item['href']
-
-      # 画像URLを取得
-      if self.check_img_url(img_url) then
-        img_url_array.push(img_url)
-      end
-    end
-
-    # URLの重複をなくす
-    img_url_array.uniq!
-
-    # URLの配列について処理
-    img_url_array.each do |value|
-      # 画像のタイトルを決定
-      img_title = ""
-      if /^.+\/(.*)\..*$/ =~ value then
-        img_title = "futaba_" + $1
-      end
-
-      # 出力テスト
-      puts printf("%s : %s", img_title, value)
-
-      # Imageモデル生成＆DB保存
-      Scrape::save_image(img_title, value)
+      break if count >= limit
     end
   end
 
-
-  # 画像のURLであることを確認する関数
-  def self.check_img_url(img_url)
-    # jpg, png, gif形式以外ならば除外する
-    if !(/\.jpg$/i =~ img_url or /\.png$/i =~ img_url or /\.gif$/i =~ img_url) then
-      return false
-    end
-
-    # サムネイル画像のURLを除外
-    if /s\.(.*)$/ =~ img_url then
-      return false
-    end
-
-    true
+  def self.get_data(thread, post)
+    {
+      title: post.title,
+      caption: post.body,
+      site_name: '2chan',
+      src_url: post.image.uri,
+      page_url: thread.uri,
+      posted_at: post.date
+    }
   end
 
 end
---
+++ spec/scripts/scrape/scrape_futaba_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require "#{Rails.root}/script/scrape/scrape_2ch"
+
+describe Scrape::Futaba do
+  before do
+    #IO.any_instance.stub(:puts)
+    Resque.stub(:enqueue).and_return  # resqueにenqueueしないように
+  end
+
+  describe "scrape function" do
+    it "calls valid functions" do
+      Scrape::Futaba.stub(:scrape_threads).and_return
+      Scrape::Futaba.should_receive(:scrape_threads).exactly(1).times
+
+      Scrape::Futaba.scrape
+    end
+  end
+
+  describe "scrape_threads function" do
+    it "call valid function" do
+      threads = Scrape::Futaba.get_threads
+
+      Scrape.stub(:save_image).and_return
+      Scrape.should_receive(:save_image).at_least(1).times
+      Scrape::Futaba.stub(:get_data).and_return
+      Scrape::Futaba.should_receive(:get_data).at_least(1).times
+
+      Scrape::Futaba.scrape_threads threads, 1
+    end
+  end
+
+  describe "get_data function" do
+    it "returns valid hash" do
+      threads = Scrape::Futaba.get_threads
+      post = threads.first.posts.first
+
+      result = Scrape::Futaba.get_data threads.first, post
+      expect(result).to be_a(Hash)
+    end
+  end
+
+end
