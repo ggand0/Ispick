13165d5d949fd15d6b22bc2fb0719b858fdb1966
pentiumx <pentiumx@live.jp>
2014-06-03 00:48:43 +0900

    refs #176 Generate ImageBoard model

---
+++ app/models/image_board.rb
@@ -0,0 +1,2 @@
+class ImageBoard < ActiveRecord::Base
+end
---
+++ spec/models/image_board_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ImageBoard, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
