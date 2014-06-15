7501c142757e370f08d90c7e7e4bd90cf0223b84
pentiumx <pentiumx@live.jp>
2014-06-15 19:02:45 +0900

    refs #190 Fix syntax error of image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,7 +9,7 @@ class Image < ActiveRecord::Base
 
 	has_attached_file :data,
     styles: { thumb: "300x300#", medium: "600x800>" },
-    default_url: lambda { |data| data.instance.set_default_url}
+    default_url: lambda { |data| data.instance.set_default_url},
     use_timestamp: false
 
   before_destroy :destroy_attachment
