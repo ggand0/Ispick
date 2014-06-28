96e376c6a2078d80639381cf2f8dd6da90752c84
pentiumx <pentiumx@live.jp>
2014-06-28 22:11:49 +0900

    refs #212 Configure resque_logger gem

--- app/workers/download_image.rb
+++ app/workers/download_image.rb
@@ -1,33 +1,38 @@
 #-*- coding: utf-8 -*-
 class DownloadImage
-  # Woeker起動時に指定するQUEUE名
-  @queue = :download_image
+  extend Resque::Plugins::Logger
 
-  # 画像をDLする
+  @queue = :download_image          # Woeker起動時に指定するQUEUE名
+  @log_name = 'download_image.log'  # Loggerが書き込むファイル名。デフォルトではworker名
+
+  # 画像をWebからダウンロードしてpaperclipのattachmentを設定する
+  # @param image_type [String] 画像オブジェクトのクラス名
+  # @param image_id [Integer] テーブル内のID
+  # @param src_url [String] Source url
   def self.perform(image_type, image_id, src_url)
     image = Object::const_get(image_type).find(image_id)
 
     begin
-      # 画像ダウンロード
+      # 画像をsource urlからダウンロード
       image.image_from_url src_url
 
       # DeliveredImageの場合は、そのままイラスト判定へ
       # Imageオブジェクトの場合は、全く同一のファイルを持つレコードが既にDBに存在すれば削除する
       if image.kind_of? Image and Image.where(md5_checksum: image.md5_checksum).count > 0
         Image.destroy(image_id)
-        puts "Destroyed duplicates : #{image_type}/#{image_id}"
+        logger.info "Destroyed duplicates : #{image_type}/#{image_id}"
       else
         # それ以外(含Image)はmd5_checksumを保存した後イラスト判定処理を行う
         image.save!
+        logger.info "Downloaded : #{image_type}/#{image_id}"
+
         Resque.enqueue(DetectIllust, image_type, image.id)
         Resque.enqueue(ImageFace, image_type, image.id)
       end
     rescue => e
-      puts e
-      Rails.logger.error('Image download failed!')
-      return
+      Resque.logger.info e
+      logger.error('Image download failed!')
     end
-
-    puts "Downloaded : #{image_type}/#{image_id}"
   end
+
 end
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -7,3 +7,14 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 Resque.redis = resque_config[rails_env]
+
+# Configure logger
+log_path = File.join Rails.root, 'log'
+config = {
+  folder:     log_path,                 # destination folder
+  class_name: Logger,                   # logger class name
+  #class_args: [ 'daily', 1.kilobyte ],  # logger additional parameters
+  level:      Logger::INFO,             # optional
+  formatter:  Logger::Formatter.new,    # optional
+}
+Resque.logger_config = config
