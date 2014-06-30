b09d2321272473d75d8e02f0fea29f555262dc79
pentiumx <pentiumx@live.jp>
2014-07-01 05:01:30 +0900

    refs #212 Change logger's level option

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -11,7 +11,7 @@ config = {
   folder:     log_path,                 # destination folder
   class_name: Logger,                   # logger class name
   #class_args: [ 'daily', 1.kilobyte ],  # logger additional parameters
-  level:      Logger::INFO,             # optional
+  level:      Logger::DEBUG,             # optional
   formatter:  ActiveSupport::Logger::SimpleFormatter.new,    # optional
 }
 Resque.logger_config = config
