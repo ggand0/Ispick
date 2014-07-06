8c39889ae95d176cc5719c77bb8dac14040671c7
pentiumx <pentiumx@live.jp>
2014-07-06 18:27:37 +0900

    refs #223 Fix the association definition

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,4 @@
 class Keyword < ActiveRecord::Base
-  has_and_belongs_to_many :people
+  has_and_belongs_to_many :people, :join_table => 'people_keywords'
   validates_uniqueness_of :word
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
   belongs_to :target_word
-  has_and_belongs_to_many :keywords
+  has_and_belongs_to_many :keywords, :join_table => 'people_keywords'
   has_and_belongs_to_many :titles
 
   validates_uniqueness_of :name
