e5060979dfc0b7e44cf55a113b70e1ad8d8c21fd
pentiumx <pentiumx@live.jp>
2014-07-11 19:48:01 +0900

    refs #214 Replace Nico module with Nico class

--- script/scrape/scrape_nico.rb
+++ script/scrape/scrape_nico.rb
@@ -1,173 +1,172 @@
 # coding: utf-8
+require "#{Rails.root}/script/scrape/client"
 require 'open-uri'
 
 
-module Scrape::Nico
-  RSS_URL = 'http://seiga.nicovideo.jp/rss/illust/new'
-  TAG_SEARCH_URL = 'http://seiga.nicovideo.jp/api/tagslide/data'
-  ROOT_URL = 'http://seiga.nicovideo.jp'
-
-  # Scrape images from nicoseiga. The latter two params are used for testing.
-  # @param [Integer] min
-  # @param [Boolean] whether it's called for debug or not
-  # @param [Boolean] whether it's called for debug or not
-  def self.scrape(interval=60, pid_debug=false, sleep_debug=false)
-    limit = 50
-    logger = Logger.new('log/scrape_nico_cron.log')
-    logger.formatter = ActiveSupport::Logger::SimpleFormatter.new
-    Scrape.scrape_target_words('Scrape::Nico', logger, limit, interval, pid_debug, sleep_debug)
-  end
+module Scrape
+  class Nico < Client
+    RSS_URL = 'http://seiga.nicovideo.jp/rss/illust/new'
+    TAG_SEARCH_URL = 'http://seiga.nicovideo.jp/api/tagslide/data'
+    ROOT_URL = 'http://seiga.nicovideo.jp'
 
 
-  # キーワードによる検索・抽出を行う
-  # @param target_word[TargetWord]
-  # @param logger [Logger] ログ出力に使用させたいloggerインスタンス
-  def self.scrape_target_word(user_id, target_word, logger)
-    limit = 10
-    logger.info "Extracting #{limit} images from: #{ROOT_URL}"
+    def initialize(logger=nil, limit=50)
+      self.limit = limit
 
-    result = self.scrape_using_api(user_id, target_word, limit, logger, true)
-    logger.info "scraped: #{result[:scraped]}, duplicates: #{result[:duplicates]}, avg_time: #{result[:avg_time]}"
-  end
-
-  # キーワードからタグ検索してlimit分の画像を保存する
-  # @param [String]
-  # @param [Integer]
-  # @param [Boolean]
-  # @return [Hash]
-  def self.scrape_using_api(target_word, limit, logger, user_id=nil, validation=true, logging=false)
-    # nilのクエリは弾く
-    query = Scrape.get_query target_word
-    logger.info "query=#{query}"
-    return if query.nil? or query.empty?
-
-    agent = self.get_client
-    url = "#{TAG_SEARCH_URL}?page=1&query=#{query}"
-    escaped = URI.escape(url)
-    xml = agent.get(escaped)
-    duplicates = 0
-    scraped = 0
-    avg_time = 0
-
-    # 画像情報を取得してlimit枚DBヘ保存する
-    xml.search('image').take(limit).each_with_index do |item, count|
-      begin
-        next if item.css('adult_level').first.content.to_i > 0  # 春画画像をskip
-
-        start = Time.now
-        image_data = self.get_data(item)                        # APIの結果から画像情報取得
-        image_id = Scrape::save_image(image_data, logger, [ self.get_tag(query) ] , validation, false, false, false)
-
-        duplicates += image_id ? 0 : 1
-        scraped += 1 if image_id
-        elapsed_time = Time.now - start
-        avg_time += elapsed_time
-        logger.info "Scraped from #{image_data[:src_url]} in #{elapsed_time} sec" if logging and image_id
-
-        # Resqueで非同期的に画像解析を行う
-        # 始めに画像をダウンロードし、終わり次第ユーザに配信
-        if image_id
-          Scrape.generate_jobs(image_id, image_data[:src_url], false, user_id, target_word.class.name, target_word.id)
-        end
-
-        break if duplicates >= 3
-      rescue => e
-        # 検索結果が0の場合など
-        logger.error e
-        next
+      if logger.nil?
+        self.logger = Logger.new('log/scrape_nico_cron.log')
+      else
+        self.logger = logger
       end
-      break if count+1 >= limit
+      self.logger.formatter = ActiveSupport::Logger::SimpleFormatter.new
     end
 
-    { scraped: scraped, duplicates: duplicates, avg_time: avg_time / ((scraped+duplicates)*1.0) }
-  end
-
-  # Image modelのattributesを組み立てる
-  # @param [Nokogiri::HTML]
-  # @return [Hash]
-  def self.get_data(item)
-    {
-      title: item.css('title').first.content,
-      caption: item.css('description').first.content,
-      src_url: "http://lohas.nicoseiga.jp/thumb/#{item.css('id').first.content}i",
-      page_url: "http://seiga.nicovideo.jp/seiga/im#{item.css('id').first.content}",
-      views: item.css('view_count').first.content,
-      favorites: item.css('clip_count').first.content,
-      # JSTの投稿日時が返却されるのでUTCに変換する
-      posted_at: DateTime.parse(item.css('created').first.content).in_time_zone('Asia/Tokyo').utc,
-      site_name: 'nicoseiga',
-      module_name: 'Scrape::Nico',
-    }
-  end
-
-  # page_urlから情報・画像抽出する
-  # @param [String]
-  # @param [Mechanize]
-  # @param [Hash]
-  # @param [Boolean]
-  def self.get_contents(page_url, agent, image_data, validation=true)
-    start = Time.now
-    begin
-      page = agent.get(page_url)  # 元ページを開く
-    rescue Exception => e         # ログイン求められて失敗した場合など
-      puts "Failed to open page_url: #{page_url}"
-      puts e
-      Rails.logger.info('Could not open a page.')
-      return
+    # Scrape images from nicoseiga. The latter two params are used for testing.
+    # @param [Integer] min
+    def scrape(interval=60)
+      scrape_target_words('Scrape::Nico', interval)
     end
 
-    # タグ情報を取得
-    tag_string = page.at("meta[@name='keywords']").attr('content')
-    tags = self.get_tags(tag_string.split(','))
 
-    # Imageレコードをupdate
-    #Scrape::save_image(hash.merge(image_data), tags, validation)
+    # キーワードによる検索・抽出を行う
+    # @param target_word[TargetWord]
+    def scrape_target_word(user_id, target_word)
+      @limit = 10
+      @logger.info "Extracting #{limit} images from: #{ROOT_URL}"
 
-    puts "Updated in #{(Time.now - start).to_s} sec"
-  end
-
-  # Mechanizeによるログインを行う
-  # @return [Mechanize] Mechanizeのインスタンスを初期化して返す
-  def self.get_client
-    agent = Mechanize.new
-    agent.ssl_version = 'SSLv3'
-    agent.post('https://secure.nicovideo.jp/secure/login?site=seiga',
-      'mail' => CONFIG['nico_email'],'password' => CONFIG['nico_password'])
-    agent
-  end
-
-  # タグを取得する。DBに既にある場合はそのレコードを返す
-  # @param [String]
-  def self.get_tag(tag)
-    t = Tag.where(name: tag)
-    t.empty? ? Tag.new(name: tag) : t.first
-  end
-
-  # タグを取得する。DBに既にある場合はそのレコードを返す
-  # @param [Array]
-  def self.get_tags(tags)
-    tags.map do |tag|
-      t = Tag.where(name: tag)
-      t.empty? ? Tag.new(name: tag) : t.first
-    end
-  end
-
-  # delivered_images update用に、
-  # ログインしてstats情報だけ返す関数
-  # @param [Mechanize]
-  # @param [String]
-  # @return [Hash]
-  def self.get_stats(agent, page_url)
-    begin
-      page = agent.get(page_url)
-      info_elements = page.at("ul[@class='illust_count']")
-      views = info_elements.css("li[class='view']").css("span[class='count_value']").first.content
-      comments = info_elements.css("li[class='comment']").css("span[class='count_value']").first.content
-      clips = info_elements.css("li[class='clip']").css("span[class='count_value']").first.content
-    rescue => e
-      return false
+      result = scrape_using_api(target_word, user_id, true)
+      @logger.info "scraped: #{result[:scraped]}, duplicates: #{result[:duplicates]}, avg_time: #{result[:avg_time]}"
     end
 
-    { views: views, favorites: clips}
+    # キーワードからタグ検索してlimit分の画像を保存する
+    # @param [String]
+    # @param [Integer]
+    # @param [Boolean]
+    # @return [Hash]
+    def scrape_using_api(target_word, user_id=nil, validation=true, verbose=false)
+      # nilのクエリは弾く
+      query = Scrape.get_query target_word
+      return if query.nil? or query.empty?
+
+      @logger.info "query=#{query}"
+      agent = self.class.get_client
+      url = "#{TAG_SEARCH_URL}?page=1&query=#{query}"
+      escaped = URI.escape(url)
+      xml = agent.get(escaped)
+      duplicates = 0
+      scraped = 0
+      avg_time = 0
+
+
+      # 画像情報を取得してlimit枚DBヘ保存する
+      xml.search('image').take(@limit).each_with_index do |item, count|
+        begin
+          next if item.css('adult_level').first.content.to_i > 0  # 春画画像をskip
+
+          start = Time.now
+          image_data = self.class.get_data(item)                        # APIの結果から画像情報取得
+          options = {
+            validation: validation,
+            large: false,
+            verbose: false,
+            resque: false
+          }
+          image_id = self.class.save_image(image_data, @logger, [ Scrape.get_tag(query) ], options)
+
+          duplicates += image_id ? 0 : 1
+          scraped += 1 if image_id
+          elapsed_time = Time.now - start
+          avg_time += elapsed_time
+          @logger.info "Scraped from #{image_data[:src_url]} in #{elapsed_time} sec" if verbose and image_id
+
+          # Resqueで非同期的に画像解析を行う
+          # 始めに画像をダウンロードし、終わり次第ユーザに配信
+          if image_id
+            Scrape.generate_jobs(image_id, image_data[:src_url], false, user_id, target_word.class.name, target_word.id)
+          end
+
+          break if duplicates >= 3
+        rescue => e
+          # 検索結果が0の場合など
+          @logger.error e
+          next
+        end
+        break if count+1 >= limit
+      end
+
+      { scraped: scraped, duplicates: duplicates, avg_time: avg_time / ((scraped+duplicates)*1.0) }
+    end
+
+    # Image modelのattributesを組み立てる
+    # @param [Nokogiri::HTML]
+    # @return [Hash]
+    def self.get_data(item)
+      {
+        title: item.css('title').first.content,
+        caption: item.css('description').first.content,
+        src_url: "http://lohas.nicoseiga.jp/thumb/#{item.css('id').first.content}i",
+        page_url: "http://seiga.nicovideo.jp/seiga/im#{item.css('id').first.content}",
+        views: item.css('view_count').first.content,
+        favorites: item.css('clip_count').first.content,
+        # JSTの投稿日時が返却されるのでUTCに変換する
+        posted_at: DateTime.parse(item.css('created').first.content).in_time_zone('Asia/Tokyo').utc,
+        site_name: 'nicoseiga',
+        module_name: 'Scrape::Nico',
+      }
+    end
+
+    # [OLD]page_urlから情報・画像抽出する
+    # @param [String]
+    # @param [Mechanize]
+    # @param [Hash]
+    # @param [Boolean]
+    def self.get_contents(page_url, agent, image_data, validation=true)
+      start = Time.now
+      begin
+        page = agent.get(page_url)  # 元ページを開く
+      rescue Exception => e         # ログイン求められて失敗した場合など
+        puts "Failed to open page_url: #{page_url}"
+        puts e
+        Rails.logger.info('Could not open a page.')
+        return
+      end
+
+      # タグ情報を取得
+      tag_string = page.at("meta[@name='keywords']").attr('content')
+      tags = Scrape.get_tags(tag_string.split(','))
+
+      puts "Updated in #{(Time.now - start).to_s} sec"
+    end
+
+    # Mechanizeによるログインを行う
+    # @return [Mechanize] Mechanizeのインスタンスを初期化して返す
+    def self.get_client
+      agent = Mechanize.new
+      agent.ssl_version = 'SSLv3'
+      agent.post('https://secure.nicovideo.jp/secure/login?site=seiga',
+        'mail' => CONFIG['nico_email'],'password' => CONFIG['nico_password'])
+      agent
+    end
+
+
+    # delivered_images update用に、
+    # ログインしてstats情報だけ返す関数
+    # @param [Mechanize]
+    # @param [String]
+    # @return [Hash]
+    def self.get_stats(agent, page_url)
+      begin
+        page = agent.get(page_url)
+        info_elements = page.at("ul[@class='illust_count']")
+        views = info_elements.css("li[class='view']").css("span[class='count_value']").first.content
+        comments = info_elements.css("li[class='comment']").css("span[class='count_value']").first.content
+        clips = info_elements.css("li[class='clip']").css("span[class='count_value']").first.content
+      rescue => e
+        return false
+      end
+
+      { views: views, favorites: clips}
+    end
+
   end
 end
--- spec/scripts/scrape/scrape_nico_spec.rb
+++ spec/scripts/scrape/scrape_nico_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 require "#{Rails.root}/script/scrape/scrape"
+require "#{Rails.root}/script/scrape/client"
 
 describe Scrape::Nico do
   let(:valid_attributes) { FactoryGirl.attributes_for(:image_url) }
   let(:xml) { IO.read(Rails.root.join('spec', 'fixtures', 'nico_api_response.xml')) }
 
   before do
-    IO.any_instance.stub(:puts)           # コンソールに出力しないようにしておく
+    #IO.any_instance.stub(:puts)           # コンソールに出力しないようにしておく
     Resque.stub(:enqueue).and_return nil  # resqueにenqueueしないように
     @agent = Scrape::Nico.get_client      # Mechanize agentの作成
+    @client = Scrape::Nico.new(nil, 10)
 
     url = 'http://seiga.nicovideo.jp/rss/illust/new'
     xml = Nokogiri::XML(open(url))
@@ -19,44 +21,26 @@ describe Scrape::Nico do
   describe "scrape method" do
     it "calls scrape_using_api method" do
       FactoryGirl.create(:word_with_person)
-      Scrape::Nico.stub(:scrape_using_api).and_return({ scraped: 0, duplicates: 0, avg_time: 0 })
-      Scrape::Nico.should_receive(:scrape_using_api)
 
-      Scrape::Nico.scrape(60, true, true)
+      @client.stub(:scrape_target_words).and_return nil
+      expect(@client).to receive(:scrape_target_words)
+
+      @client.scrape(60)
     end
 
-    it "sleeps with right interval after each scraping" do
-      FactoryGirl.create_list(:target_word, 5)
-      Scrape.should_receive(:sleep).with(10*60)      # (60-10) / 5*1.0
-      Scrape.stub(:sleep).and_return nil
-
-      Scrape::Nico.scrape(60, true, false)
-    end
-
-    it "raise error when it gets improper argument" do
-      FactoryGirl.create(:word_with_person)
-      expect { Scrape::Nico.scrape(14, false, true) }.to raise_error(Exception)
-    end
-
-    it "exit if another process is running" do
-      PidFile.stub(:running?).and_return(true)
-
-      expect {
-        Scrape::Nico.scrape(15, false, false)
-      }.to raise_error(SystemExit)
-    end
   end
 
   describe "scrape_target_word function" do
     it "calls proper functions" do
       target_word = FactoryGirl.create(:word_with_person)
-      Scrape::Nico.should_receive(:scrape_using_api)
-      Scrape::Nico.stub(:scrape_using_api).and_return({ scraped: 0, duplicates: 0, avg_time: 0 })
-      logger = Logger.new('log/scrape_nico_cron.log')
 
-      Scrape::Nico.scrape_target_word target_word, logger
+      @client.stub(:scrape_using_api).and_return({ scraped: 0, duplicates: 0, avg_time: 0 })
+      expect(@client).to receive(:scrape_using_api)
+
+      @client.scrape_target_word(1, target_word)
     end
   end
+
   describe "scrape_using_api function" do
     before do
       stream = File.read(Rails.root.join('spec', 'fixtures', 'nico_api_response.xml'))
@@ -65,52 +49,38 @@ describe Scrape::Nico do
         URI.escape(uri) ,
         body: stream,
         content_type: 'text/xml')
-      @logger = Logger.new('log/scrape_nico_cron.log')
     end
 
     it "skip if keyword arg is nil" do
       Scrape::Nico.should_not_receive(:get_data)
-      logger = Logger.new('log/scrape_nico_cron.log')
 
-      Scrape::Nico.scrape_using_api(nil, 5, logger, false)
+      @client.scrape_using_api(nil)
     end
 
     it "calls get_data function 'limit' times" do
-      limit = 50
-      #Scrape::Nico.stub(:get_data).and_return({})
+      limit = 10
+
+      Scrape::Nico.stub(:get_data).and_return({})
       Scrape::Nico.should_receive(:get_data).exactly(limit).times
-      Scrape.stub(:save_image).and_return(1)
-      Scrape.should_receive(:save_image).exactly(limit).times
+      Scrape::Client.stub(:save_image).and_return(1)
+      Scrape::Client.should_receive(:save_image).exactly(limit).times
       target_word = FactoryGirl.create(:word_with_person)
 
-      Scrape::Nico.scrape_using_api(target_word, limit, @logger, false)
+      @client.scrape_using_api(target_word)
     end
 
     it "allows duplicates three times" do
       Scrape::Nico.stub(:get_data).and_return({})
       Scrape::Nico.should_receive(:get_data).exactly(3).times
-      Scrape.stub(:save_image).and_return nil
-      Scrape.should_receive(:save_image).exactly(3).times
+      Scrape::Client.stub(:save_image).and_return nil
+      Scrape::Client.should_receive(:save_image).exactly(3).times
       target_word = FactoryGirl.create(:word_with_person)
 
-      Scrape::Nico.scrape_using_api(target_word, 3, @logger, false)
+      @client.scrape_using_api(target_word)
     end
   end
 
-  describe "get_tags function" do
-    it "returns an array of tags" do
-      tags = Scrape::Nico.get_tags(['Madoka'])
-      expect(tags).to be_an(Array)
-      expect(tags.first.name).to eql('Madoka')
-    end
-    it "uses existing tags if tags are duplicate" do
-      image = FactoryGirl.create(:image_with_specific_tags)
 
-      tags = Scrape::Nico.get_tags(['鹿目まどか'])
-      #expect(tags.first.images.first.id).to eql(tag.images.first.id)
-      expect(tags.first.images.first.id).to eql(image.id)
-    end
-  end
 
   describe "get_contents method" do
     it "creates an image model from image source" do
@@ -125,7 +95,7 @@ describe Scrape::Nico do
       url = 'An invalid page url'
 
       Rails.logger.should_receive(:info)
-      Scrape.should_not_receive(:save_image)
+      Scrape::Client.should_not_receive(:save_image)
 
       Scrape::Nico.get_contents(url, @agent, @title)
     end
