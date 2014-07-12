d74090c351489a31d2e717eefa6a33df04671e2b
pentiumx <pentiumx@live.jp>
2014-07-12 17:00:46 +0900

    refs #221 Increase the scraping frequency

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,15 +22,15 @@
 #set :output, { error: 'log/error.log', standard: 'log/cron.log'}
 
 
-every 60.minutes do
+every 2.hours do
   rake 'scrape:twitter[60]', output: { error: 'log/scrape_twitter_error.log', standard: 'log/scrape_twitter_cron.log' }
 end
 
-every 2.hours do
+every 4.hours do
   rake 'scrape:nico[120]', output: { error: 'log/scrape_nico_error.log', standard: 'log/scrape_nico_cron.log' }
 end
 
-every 3.hours do
+every 5.hours do
   rake 'scrape:tumblr[240]', output: { error: 'log/scrape_tumblr_error.log', standard: 'log/scrape_tumblr_cron.log' }
 end
 
@@ -39,16 +39,10 @@ every 6.hours do
 end
 
 # 配信システム系
-every 30.minutes do
+every 60.minutes do
   # 指定枚数を超えたらその分Imagesから削除
-  rake 'scrape:delete_excess[100000]', output: 'log/deliver.log'
+  rake 'scrape:delete_excess[500000]', output: 'log/deliver.log'
 
   # 全てのユーザーに推薦イラストを配信
   rake 'deliver:all', output: 'log/deliver.log'
-end
-
-# パフォーマンスの関係で凍結中
-every 6.hours do
-  # 配信画像の統計情報を更新する
-  #rake 'deliver:update'
 end
