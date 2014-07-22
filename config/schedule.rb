347c3e6559226a7391477c859b7d7e74c09b57fe
pentiumx <pentiumx@live.jp>
2014-07-22 13:03:43 +0900

    refs #221 Remove the whenever task which scrape from twitter

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,15 +23,15 @@
 
 
 every 2.hours do
-  rake 'scrape:twitter[60]', output: { error: 'log/scrape_twitter_error.log', standard: 'log/scrape_twitter_cron.log' }
+  #rake 'scrape:twitter[60]', output: { error: 'log/scrape_twitter_error.log', standard: 'log/scrape_twitter_cron.log' }
 end
 
 every 4.hours do
-  rake 'scrape:nico[120]', output: { error: 'log/scrape_nico_error.log', standard: 'log/scrape_nico_cron.log' }
+  rake 'scrape:nico[240]', output: { error: 'log/scrape_nico_error.log', standard: 'log/scrape_nico_cron.log' }
 end
 
 every 5.hours do
-  rake 'scrape:tumblr[240]', output: { error: 'log/scrape_tumblr_error.log', standard: 'log/scrape_tumblr_cron.log' }
+  rake 'scrape:tumblr[360]', output: { error: 'log/scrape_tumblr_error.log', standard: 'log/scrape_tumblr_cron.log' }
 end
 
 every 6.hours do
