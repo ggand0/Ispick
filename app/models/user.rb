3536bd9b67357993281af39afab83fcddb83329c
pentiumx <pentiumx@live.jp>
2014-09-02 01:20:09 +0900

    #172 Remove a has_many association of User model
    Remove the has_many :images line, plus add several comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,28 @@
 class User < ActiveRecord::Base
-  # Set this variabale true during testing.
+  # Set this variabale true during testing to skip all callbacks.
   cattr_accessor :skip_callbacks
 
-  has_many :images, dependent: :destroy
+  # has_many uploaded images
   has_many :target_images, dependent: :destroy
+
+  # has_many boards for storing clipped images
   has_many :image_boards, dependent: :destroy
 
-  # 登録タグをhas_manyしている
-  # タグが追加されたらcallbackを呼んで抽出・配信処理を行う
+  # has_many tags for making image feeds
   has_many :target_words_users
   has_many :target_words, :through => :target_words_users
 
+  # devise configuration
   devise :database_authenticatable, :omniauthable, :recoverable,
          :registerable, :rememberable, :trackable, :validatable
 
+  # paperclip configuration: thumbnail size, etc.
   has_attached_file :avatar,
     styles: { thumb: "x50" },
     default_url: lambda { |data| data.instance.set_default_url },
     use_timestamp: false
 
+  # callbacks and validations
   after_create :create_default
   validates :name, presence: true
 
@@ -27,7 +31,6 @@ class User < ActiveRecord::Base
   # ==================
   #  Instance methods
   # ==================
-
   # Scrape and deliver images right after a new tag is created by an user.
   # @param target_word [TargetWord]
   def search_keyword(target_word)
@@ -37,24 +40,6 @@ class User < ActiveRecord::Base
   # Get images which is shown at user's home page.
   # @return [ActiveRecord::AssociationRelation]
   def get_images
-=begin
-    images = target_words.first.images
-    target_words.all.each_with_index do |target_word, count|
-      next if count == 0
-      #images = images.merge(target_word.images)
-      #images += target_word.images
-      images = images + target_word.images
-    end
-
-    images = Image.where("id IN (#{images.map(&:id).join(',')})")
-
-    images.
-      where.not(is_illust: nil).        # Already downloaded
-      where.not(site_name: 'twitter').
-      #reorder('posted_at DESC')         # Sort by posted_at value
-      reorder('created_at DESC')         # Sort by posted_at value
-      .limit(200)
-=end
     words = target_words.map{ |target_word| target_word.word }
     Image.joins(:target_words).where("target_words.word IN (?)", words).where.not(is_illust: nil).references(:target_words)
   end
@@ -88,12 +73,14 @@ class User < ActiveRecord::Base
     end
   end
 
+  # Sort images by its favorites attribute.
   # @return [ActiveRecord::AssociationRelation]
   def self.sort_images(images, page)
     images = images.reorder('images.favorites desc')
     images.page(page).per(25)
   end
 
+  # Sort images by its quality attribute.
   # @return [ActiveRecord::AssociationRelation]
   def self.sort_by_quality(images, page)
     images = images.reorder('quality desc')
@@ -106,21 +93,20 @@ class User < ActiveRecord::Base
   end
 
   # Create a default image board and attach it to self instance.
+  # 新しいレコードが作成された際に実行される。デフォルトのボード作成などを行う。
   def create_default
     # generate default image_board
     image_board = ImageBoard.create(name: 'Default')
     self.image_boards << image_board
-
-    # generate default target_word
-    #target_word = TargetWord.where(word: '鹿目まどか').first
-    #target_word = TargetWord.create(word: '鹿目まどか') if target_word.nil?
-    #self.target_words << target_word
 
     # generate default avatar
     self.avatar = File.open("#{Rails.root}/app/assets/images/icepick.png")
     self.save!
   end
 
+  # Get an optional ImageBoard instance by board_id.
+  # そのUserオブジェクトに関連したImageBoardオブジェクトを取得する。
+  # board_idが指定されない場合はimage_boards内の一番最初のオブジェクトを返す。
   # @param board_id [Integer] The image_board's id which you want to retrive
   # @return [ImageBoard]
   def get_board(board_id=nil)
@@ -135,7 +121,6 @@ class User < ActiveRecord::Base
   # ===============================
   #  Authorization related methods
   # ===============================
-
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
@@ -159,7 +144,6 @@ class User < ActiveRecord::Base
     end
     user
   end
-
 
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
