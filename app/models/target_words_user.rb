0a44c2dfa8f9784c3e24c5923b44270d29bfc69f
pentiumx <pentiumx@live.jp>
2014-09-02 01:10:47 +0900

    #233 Disable the after_create callback of TargetWordsUser model

--- app/models/target_words_user.rb
+++ app/models/target_words_user.rb
@@ -3,9 +3,14 @@ class TargetWordsUser < ActiveRecord::Base
   belongs_to :user
 
   validates :target_word_id, uniqueness: { scope: :user_id }
-  after_create :search_keyword, unless: :skip_callbacks
+  #after_create :search_keyword, unless: :skip_callbacks
 
-  # 少量の画像を抽出・配信する
+  # Scrape and deliver a small amount of images from each target websites.
+  # In older version this function is called right after a TargetWordsUser record is created.
+  # Now it's only called when we need to scrape images immediately, like during debugging.
+  # 緊急に画像を抽出する必要がある時に呼ぶ（普段は専用のプロセスで間隔を空けながら全TargetWordをクロールしている）。
+  # 以前は新しいTargetWordsUserレコードが登録される度に呼んでいた（予め画像をクロールする仕様ではなかった）が、
+  # 現在はデバッグ用にしか使っていない。
   def search_keyword
     Resque.enqueue(SearchImages, self.user_id, self.target_word_id)
   end
