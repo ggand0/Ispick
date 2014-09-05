d4fb3ecc1b12ae7b40acaa913710bb48118ffdc1
pentiumx <pentiumx@live.jp>
2014-09-05 16:32:16 +0900

    #247 Add images_count column to target_words table
    Plus, organize the recent migrations.

--- db/migrate/20140902144735_add_users_count_to_target_words.rb
+++ db/migrate/20140902144735_add_users_count_to_target_words.rb
@@ -1,5 +1,7 @@
 class AddUsersCountToTargetWords < ActiveRecord::Migration
   def change
     add_column :target_words, :users_count, :integer, :default => 0, :null => false
+    add_column :target_words, :images_count, :integer, :default => 0, :null => false
+    add_column :target_words, :crawl_count, :integer, :default => 0, :null => false
   end
 end
--- db/migrate/20140903184249_add_crawl_count_to_target_words.rb
+++
@@ -1,5 +0,0 @@
-class AddCrawlCountToTargetWords < ActiveRecord::Migration
-  def change
-    add_column :target_words, :crawl_count, :integer, :default => 0, :null => false
-  end
-end
