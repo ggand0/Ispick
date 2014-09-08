7b5d3b0778665ea96da951cf60e1bc11d675723f
pentiumx <pentiumx@live.jp>
2014-09-08 15:27:49 +0900

    #149 Fix the script again

--- script/anidb.rb
+++ script/anidb.rb
@@ -19,11 +19,12 @@ titles = Title.all
 match_count = 0
 hash = {}
 titles.each do |title|
-  hash[title.name] = false
+  hash[title.name_english] = false
 end
 
 xml.search('anime').each_with_index do |anime, count|
   anime_ens = anime.xpath('title[@xml:lang="en"]')
+  anime_jas = anime.xpath('title[@xml:lang="ja"]')
   anime_mains = anime.xpath('title[@xml:lang="x-jat"]')
   anime_officials = anime.xpath('title[@xml:lang="x-unk"]')
   next if anime_ens.nil? and anime_mains.nil? and anime_officials.nil?
@@ -32,26 +33,41 @@ xml.search('anime').each_with_index do |anime, count|
   titles.each do |title|
 
     anime_ens.each do |anime_en|
-      if not anime_en.nil? and (anime_en.content.downcase.include? title.name.downcase or
-        title.name.downcase.include? anime_en.content.downcase)
-      #if anime_en.content.casecmp(title.name) == 1 or anime_title.content.casecmp(title.name) == 0
-      #if anime_en.content.any?{ |s| s.casecmp(title.name)==0 }
+      if not anime_en.nil? and (anime_en.content.downcase.include? title.name_english.downcase or
+        title.name_english.downcase.include? anime_en.content.downcase)
+      #if anime_en.content.casecmp(title.name_english) == 1 or anime_title.content.casecmp(title.name_english) == 0
+      #if anime_en.content.any?{ |s| s.casecmp(title.name_english)==0 }
         output.root.add_child anime
         puts "Added an en element: #{anime_en.content}"
         match_count += 1
-        hash[title.name] = true
+        hash[title.name_english] = true
         matched = true
         break
       end
     end
     break if matched
 
+    if title.name
+      anime_jas.each do |anime_ja|
+        if not anime_ja.nil? and (anime_ja.content.downcase.include? title.name.downcase or
+          title.name.downcase.include? anime_ja.content.downcase)
+          output.root.add_child anime
+          puts "Added an ja element: #{anime_ja.content}"
+          match_count += 1
+          hash[title.name_english] = true
+          matched = true
+          break
+        end
+      end
+      break if matched
+    end
+
     anime_mains.each do |anime_main|
-      if not anime_main.nil? and (anime_main.content.downcase.include? title.name.downcase or
-        title.name.downcase.include? anime_main.content.downcase)
+      if not anime_main.nil? and (anime_main.content.downcase.include? title.name_english.downcase or
+        title.name_english.downcase.include? anime_main.content.downcase)
         output.root.add_child anime
         puts "Added an main element: #{anime_main.content}"
-        hash[title.name] = true
+        hash[title.name_english] = true
         match_count += 1
         matched = true
         break
@@ -60,10 +76,10 @@ xml.search('anime').each_with_index do |anime, count|
     break if matched
 
     anime_officials.each do |anime_official|
-      if not anime_official.nil? and (anime_official.content.downcase.include? title.name.downcase or
-        title.name.downcase.include? anime_official.content.downcase)
+      if not anime_official.nil? and (anime_official.content.downcase.include? title.name_english.downcase or
+        title.name_english.downcase.include? anime_official.content.downcase)
         output.root.add_child anime
-        hash[title.name] = true
+        hash[title.name_english] = true
         puts "Added an official element: #{anime_official.content}"
         match_count += 1
         matched = true
