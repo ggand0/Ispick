af2168c6fbc9ba78952988684d4a9a37dbf5d554
pentiumx <pentiumx@live.jp>
2014-09-10 19:23:50 +0900

    #274 Fix a seed script

--- db/seeds/target_words.rb
+++ db/seeds/target_words.rb
@@ -1,17 +1,17 @@
 =begin
 # Seed independent tags(like "黒髪", "ポニーテール")
 File.open("#{Rails.root}/db/seeds/words_list").read.each_line do |line|
-  target_word = TargetWord.create(word: line, user_id: 1)
+  target_word = TargetWord.create(name: line, user_id: 1)
 
-  puts "Seeded: #{target_word.word}"
+  puts "Seeded: #{target_word.name}"
 end
 =end
 
 # Seed target_words based on all person records.
 # Assume people table is already seeded.
 Person.all.each do |person|
-  target_word = TargetWord.create(word: person.name)
+  target_word = TargetWord.create(name: person.name)
   target_word.person = person
 
-  puts "Seeded: #{target_word.word}"
+  puts "Seeded: #{target_word.name}"
 end
