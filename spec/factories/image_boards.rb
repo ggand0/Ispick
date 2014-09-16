c146b9d701a65a9c70f89541f00bb9d3e10b3806
pentiumx <pentiumx@live.jp>
2014-09-17 03:45:52 +0900

    #288 Add specs of the boards page

--- spec/factories/image_boards.rb
+++ spec/factories/image_boards.rb
@@ -1,16 +1,23 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  # Defaultで生成されるBoard
+
+  factory :image_board_min, class: ImageBoard do
+    name 'A board without images'
+  end
+
+  # Defaultで生成されるBoardのfactory
+  # This one generates boards used as the default board of a user
   factory :image_board do
-    name "Default"
+    name 'Default'
 
     after(:create) do |image_board|
       1.times { create(:favored_image_file, image_board: image_board) }
     end
   end
 
-  # Added by user
+  # ユーザによって新たに作成されるBoardのfactory
+  # Creates custom ImageBoard objects
   factory :image_boards, class: ImageBoard do
     sequence(:name) { |n| "My board#{n}" }
 
