66f3a03902d56b4b707c02763e81f5b5b6b2aa85
pentiumx <pentiumx@live.jp>
2014-09-19 05:07:00 +0900

    #282 Update UsersHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,33 +1,9 @@
 module UsersHelper
-  # ======================
-  #  HTML related helpers
-  # ======================
-  # Returns html code for debugging.
-  # @params image [Image] An Image object.
-  # @return [String] html code with is_illust value.
-  def get_illust_html(image)
-    return 'Illust: nil' if image.nil?
-    "Illust: <span><strong>#{image.is_illust.to_s}</strong></span>".html_safe
-  end
 
-  # Returns html code for debugging.
-  # @params image [Image] An Image object.
-  # @return [String] html code with quality value.
-  def get_quality_html(image)
-    return 'Quality: nil' if image.nil?
-    "Quality: <span><strong>#{image.quality.to_s}</strong></span>".html_safe
-  end
-
-  # Returns html code for debugging.
-  # @params image [ActiveRecord::AssociationRelation] A relation object of Image class.
-  # @return [String] html code with the count of input relation.
-  def get_debug_html(images)
-    "<strong>Found #{images.count} images.</strong>".html_safe
-  end
-
-  # ===================
-  #  Rendering helpers
-  # ===================
+  # ========================================================
+  #  Bootstrap button helpers
+  #  gem: https://github.com/Tretti/rails-bootstrap-helpers
+  # ========================================================
   # Renders a image with the link.
   # @params image [Image] An object of Image.
   # @params image [Image] An object of Image which is included in the image.
@@ -39,7 +15,7 @@ module UsersHelper
   # Renders a bootstrap button with the link.
   # @params image [Image] An object of Image.
   def render_clip_button(image)
-    bs_button_to 'Clip', { controller: 'image_boards', action: 'boards', remote: true, image: image.id, id: "popover-board#{image.id}", class: 'popover-board btn-info btn-sm' }, 'data-toggle' => "popover", 'data-placement'=>'bottom', 'data-container'=> 'body', id: "popover-board#{image.id}"
+    bs_button_to paperclip_glyphicon, { controller: 'image_boards', action: 'boards', remote: true, image: image.id, id: "popover-board#{image.id}", class: 'popover-board btn-info btn-sm' }, 'data-toggle' => "popover", 'data-placement'=>'bottom', 'data-container'=> 'body', id: "popover-board#{image.id}"
   end
 
   # Renders a bootstrap button with the link.
@@ -65,9 +41,40 @@ module UsersHelper
   end
 
 
-  # =======
-  #  DEBUG
-  # =======
+  # ==========================================
+  #  Glyphicon helpers
+  #  see: http://getbootstrap.com/components/
+  # ==========================================
+  def paperclip_glyphicon
+    '<span class="glyphicon glyphicon-paperclip" style="vertical-align:middle"></span>'.html_safe
+  end
+  def resize_full_glyphicon
+    '<span class="glyphicon glyphicon-resize-full" style="vertical-align:middle"></span>'.html_safe
+  end
+  def thumbs_up_glyphicon
+    '<span class="glyphicon glyphicon-thumbs-up" style="vertical-align:middle"></span>'.html_safe
+  end
+  def thumbs_down_glyphicon
+    '<span class="glyphicon glyphicon-thumbs-down" style="vertical-align:middle"></span>'.html_safe
+  end
+  def share_glyphicon
+    '<span class="glyphicon glyphicon-share" style="vertical-align:middle"></span>'.html_safe
+  end
+  def tags_glyphicon
+    '<span class="glyphicon glyphicon-tags" style="vertical-align:middle"></span>'.html_safe
+  end
+  def th_glyphicon
+    '<span class="glyphicon glyphicon-th" style="vertical-align:middle"></span>'.html_safe
+  end
+  def wrench_glyphicon
+    '<span class="glyphicon glyphicon-wrench" style="vertical-align:middle"></span>'.html_safe
+  end
+
+
+  # =================
+  #  DEBUG BUTTONS
+  #  will be deleted
+  # =================
   def render_clip_debug_button(image)
     bs_button_to 'Clip', { controller: 'image_boards', action: 'boards_another', remote: true, image: image.id, id: "popover-board#{image.id}",
     class: 'popover-board btn-info btn-xs' }, 'data-toggle' => "popover", 'data-placement'=>'bottom', 'data-container'=> 'body', id: "popover-board#{image.id}"
@@ -75,5 +82,33 @@ module UsersHelper
   def render_show_another_button(image)
     bs_button_to 'Show', { controller: 'images', action: 'show', id: image.id.to_s, remote: true, 'data-toggle' => "modal", 'data-target' => '#modal-image' },
     class: 'btn-default btn-xs'
+  end
+
+
+  # ======================
+  #  HTML related helpers
+  #  (just for debugging)
+  # ======================
+  # Returns html code for debugging.
+  # @params image [Image] An Image object.
+  # @return [String] html code with is_illust value.
+  def get_illust_html(image)
+    return 'Illust: nil' if image.nil?
+    "Illust: <span><strong>#{image.is_illust.to_s}</strong></span>".html_safe
+  end
+
+  # Returns html code for debugging.
+  # @params image [Image] An Image object.
+  # @return [String] html code with quality value.
+  def get_quality_html(image)
+    return 'Quality: nil' if image.nil?
+    "Quality: <span><strong>#{image.quality.to_s}</strong></span>".html_safe
+  end
+
+  # Returns html code for debugging.
+  # @params image [ActiveRecord::AssociationRelation] A relation object of Image class.
+  # @return [String] html code with the count of input relation.
+  def get_debug_html(images)
+    "<strong>Found #{images.count} images.</strong>".html_safe
   end
 end
