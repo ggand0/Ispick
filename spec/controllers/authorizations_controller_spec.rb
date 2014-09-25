f5fdb5fb4dab46bd8e7e151cae4edbbce5d350b2
pentiumx <pentiumx@live.jp>
2014-09-26 08:46:40 +0900

    #287 Update AuthorizationsController's specs
    - Delete unused methods at Authorization model

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe AuthorizationsController do
 
+  let(:valid_session) { {} }
+
   describe "GET 'destroy'" do
     it "destroys the requested authorization" do
-      ization = FactoryGirl.create(:authorization)
+      authorization = FactoryGirl.create(:authorization)
       expect {
         delete :destroy, {:id => authorization.to_param}, valid_session
       }.to change(Authorization, :count).by(-1)
     end
 
     it "redirects to the authorizations list" do
-      ization = FactoryGirl.create(:authorization)
+      authorization = FactoryGirl.create(:authorization)
       delete :destroy, {:id => authorization.to_param}, valid_session
       response.should redirect_to(settings_users_path)
     end
