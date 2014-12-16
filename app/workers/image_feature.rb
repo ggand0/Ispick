4f6997471294a18f6065e233649a3d0d2da8cfa2
pentiumx <pentiumx@live.jp>
2014-12-17 00:43:55 +0900

    #353 Implement feature extraction with caffe

--- app/workers/image_feature.rb
+++ app/workers/image_feature.rb
@@ -20,57 +20,51 @@ class ImageFeature
     file_name
   end
 
+  def self.exec_script(image)
+    # .caffemodel file
+    pretrained_file = "caffenet_train_pmmm350_iter_6000.caffemodel"
+    pretrained_path = "#{CONFIG['caffe_path']}/models/ispick/#{pretrained_file}"
+    # .npy file converted from .binaryproto file which was used for training
+    mean_file_path = "#{CONFIG['caffe_path']}/data/ispick/pmmm350_mean.npy"
+    model_file_path = "#{Rails.root}/script/caffe/imagenet_features_template.prototxt"
+
+    #Dir.mktmpdir do |temp_dir_path|
+    # Memo: feature extraction with C++ (WIP)
+    #tool_exec = "./build/tools/extract_features.bin models/ispick/caffenet_train_pmmm350_iter_2000.caffemodel features/imagenet_val.prototxt fc7 #{temp_dir_path} 1"
+    #result = %x('cd' #{CONFIG['caffe_path']}; #{tool_exec})
+
+    # Configure prototxt file
+    #model_file_path = self.deploy_prototxt(image, temp_dir_path, mean_file_path)
+
+    # Execute the script and extract feature vectors
+    dump_exec = "python #{CONFIG['caffe_path']}/extract_features.py #{mean_file_path} #{model_file_path} #{pretrained_path} #{image.data.path}"
+    result = %x('cd' #{CONFIG['caffe_path']}; #{dump_exec})
+
+    result
+  end
+
 
   # @param [Image/TargetImage] A record of TargetImage model which has already saved.
   # @return [Hash] ImageNet categories included.
   def self.extract_features(image)
-    #model_path = ""       # .prototxt file for feature extraction
-    pretrained_file = "caffenet_train_pmmm350_iter_6000.caffemodel"
-    pretrained_path = "#{CONFIG['caffe_path']}/models/Ispick/#{pretrained_file}"  # .caffemodel file
-    mean_file_path = "#{CONFIG['caffe_path']}/data/Ispick/pmmm350_mean.npy"                           # .npy file converted from .binaryproto file which was used for training
-    #script_path = "/home/"      # Path of feature_extraction.py
+    result = self.exec_script(image)
+    features = result[0..-2].split(' ')
 
-    Dir.mktmpdir do |temp_dir_path|
-      # Memo: feature extraction with C++ (WIP)
-      #tool_exec = "./build/tools/extract_features.bin models/ispick/caffenet_train_pmmm350_iter_2000.caffemodel features/imagenet_val.prototxt fc7 #{temp_dir_path} 1"
-      #result = %x('cd' #{CONFIG['caffe_path']}; #{tool_exec})
+    #hash = {}
+    #(0..4095).each do |key|
+    #  hash[key.to_s] = features[key].to_f
+    #end
 
-      # Configure prototxt file
-      model_file_path = self.deploy_prototxt(image, temp_dir_path, mean_file_path)
-
-      # Execute the script and extract feature vectors
-      dump_exec = "python #{CONFIG['caffe_path']}/extract_features.py #{mean_file_path} #{model_file_path} #{pretrained_path}"
-      result = %x('cd' #{CONFIG['caffe_path']}; #{dump_exec})
-      arr = result[0..-2].split(',')
-    end
-    
-=begin
-    hash = {}
-    result.split("\n").each do |line|
-      tmp = line.split(',')
-      hash[tmp[1]] = tmp[0].to_f
-    end
-
-    (0..4095).each do |key|
-      hash[key.to_s] = 0 if not hash.has_key?(key.to_s)
-    end
-    hash.delete(nil)
-=end
-
-    hash
+    { 'features' => features }
   end
 
   def self.perform(image_type, image_id)
     image = Object::const_get(image_type).find(image_id)
-    logger.info json_string = self.get_categories(image).to_json
-    feature = Feature.new(categ_imagenet: json_string)
+    logger.info json_string = self.extract_features(image).to_json
+    puts json_string
 
-    Feature.transaction do
-      feature.save!
-      Image.transaction do
-        image.feature = feature
-      end
-    end
+    feature = Feature.new(convnet_feature: json_string)
+    image.feature = feature
 
     logger.info 'IMAGE : FEATURE EXTRACTION DONE!'
   end
