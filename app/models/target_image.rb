04f38444bdb88e49dbe3c8fd73661d093a5eba7d
pentiumx <pentiumx@live.jp>
2014-12-18 00:51:40 +0900

    #353 Edit models

--- app/models/target_image.rb
+++ app/models/target_image.rb
@@ -9,12 +9,33 @@ class TargetImage < ActiveRecord::Base
 
   has_attached_file :data,
     styles: {
-      thumb: "100x100#",
-      small: "150x150>",
-      medium: "200x200" },
+      thumb: "300x",
+      medium: "600x800>"
+    },
     use_timestamp: false
 
   validates_presence_of :data
   validates_attachment_size :data, less_than: 5.megabytes
   validates_attachment_content_type :data, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/
+
+
+  def get_similar_convnet_images(limit=1000)
+    images = Image.all.limit(limit)
+    similar = []
+    my_feature = JSON.parse(self.feature.convnet_feature)
+
+    images.each do |image|
+      image_feature = JSON.parse(image.feature.convnet_feature)
+      
+    end
+  end
+
+  def self.get_distance(v1, v2)
+    sum = 0
+    (0..v1.count-1).each do |index|
+      sum += (v1[index]-v2[index])*(v1[index]-v2[index])
+    end
+    Math.sqrt(sum)
+  end
+
 end
