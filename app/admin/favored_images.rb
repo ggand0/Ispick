ec9d4f435ad16df0d31554b4a2f13b923c6211d2
pentiumx <pentiumx@live.jp>
2014-12-28 14:45:08 +0900

    Fix ActiveAdmin templates

--- app/admin/favored_images.rb
+++ app/admin/favored_images.rb
@@ -1,11 +1,20 @@
 ActiveAdmin.register FavoredImage do
   index do
+    actions
+
+    column :id
     column :src_url
     column :page_url
     column :original_url
     column :image_id
     column :created_at
+  end
 
-    actions
+  show do |image|
+    attributes_table do
+      row 'FavoredImage' do
+        image_tag image.data.url(:thumb)
+      end
+    end
   end
 end
