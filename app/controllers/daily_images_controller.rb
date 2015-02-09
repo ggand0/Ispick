64babce274399276d7d3e0e40e412213ad2da946
pentiumx <pentiumx@live.jp>
2015-02-09 13:05:46 +0900

    #372 Show daily images when it has html requests

--- app/controllers/daily_images_controller.rb
+++ app/controllers/daily_images_controller.rb
@@ -1,9 +1,15 @@
 class DailyImagesController < ApplicationController
   def rss
-    @images = DailyImage.all
+    display_num = current_user ? current_user.display_num : User::DEFAULT_DISPLAY_NUM
+    ids = DailyImage.all.map{ |i| i.image_id }
+    @images = Image.where('id in (?)', ids)
+    .page(params[:page]).per(display_num)
+    @count = @images.count
+    @pagination = current_user ? current_user.pagination : false
 
     respond_to do |format|
-      format.html { render nothing: true }
+      format.html {}
+      format.js {}
       format.rss { render :layout => false }
     end
   end
